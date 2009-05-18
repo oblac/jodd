86ce3ba88edbb36988fa2838ed60ab147657c005
Igor <igor@jodd.org>
2009-05-18 22:43:25 +0000

    minor change

--- mod/samples/src/examples/util/AddClassPath.java
+++ mod/samples/src/examples/util/AddClassPath.java
@@ -8,17 +8,17 @@ import jodd.io.FileUtil;
 
 public class AddClassPath {
 	public static void main(String args[]) throws Exception {
-		String working = SystemUtil.getUserDir();
+		String working = "d:\\Projects\\java\\apps\\jodd\\mod\\samples\\";
 
 
-		byte[] classBytes = FileUtil.readBytes(working + "/mod/samples/TestClass.class");
-		Class c = ClassLoaderUtil.defineClass("TestClass", classBytes);
-		System.out.println(c);
+//		byte[] classBytes = FileUtil.readBytes(working + "/mod/samples/TestClass.class");
+//		Class c = ClassLoaderUtil.defineClass("TestClass", classBytes);
+//		System.out.println(c);
 
 
-//		ClassLoaderUtil.addClassPath(working);
-//		c = Class.forName("TestClass");
-//		Object o = c.newInstance();
-//		System.out.println(o);
+		ClassLoaderUtil.addFileToClassPath(working);
+		Class c = Class.forName("TestClass");
+		Object o = c.newInstance();
+		System.out.println(o);
 	}
 }
