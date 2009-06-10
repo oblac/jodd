dd97e31cc6e867834120731dfb057e57a6e0203f
Igor <igor@jodd.org>
2009-06-10 14:25:01 +0000

    ThreadDbSessionProvider may create DbThreadSession

--- mod/jodd-wot/src/jodd/db/ThreadDbSessionProvider.java
+++ mod/jodd-wot/src/jodd/db/ThreadDbSessionProvider.java
@@ -4,9 +4,21 @@ package jodd.db;
 
 /**
  * Returns {@link DbSession} assigned to current thread.
+ * If session is not assigned to current thread, an exception is thrown, or,
+ * optionally, new {@link jodd.db.DbThreadSession thread session} is created and returned. 
  * @see jodd.db.ThreadDbSessionHolder
  */
 public class ThreadDbSessionProvider implements DbSessionProvider {
+
+	protected final boolean createIfMissing;
+
+	public ThreadDbSessionProvider() {
+		this(false);
+	}
+
+	public ThreadDbSessionProvider(boolean createIfMissing) {
+		this.createIfMissing = createIfMissing;
+	}
 
 	/**
 	 * {@inheritDoc}
@@ -14,6 +26,9 @@ public class ThreadDbSessionProvider implements DbSessionProvider {
 	public DbSession getDbSession() {
 		DbSession session = ThreadDbSessionHolder.get();
 		if (session == null) {
+			if (createIfMissing) {
+				return new DbThreadSession();
+			}
 			throw new DbSqlException("No session associated to current thread.");
 		}
 		return session;
