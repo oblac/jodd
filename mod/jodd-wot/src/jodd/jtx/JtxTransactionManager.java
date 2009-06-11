e5e743f4221811be79622244e2f2dcccd28ca485
Igor <igor@jodd.org>
2009-06-11 21:51:08 +0000

    javadoc upgrade

--- mod/jodd-wot/src/jodd/jtx/JtxTransactionManager.java
+++ mod/jodd-wot/src/jodd/jtx/JtxTransactionManager.java
@@ -72,10 +72,15 @@ public class JtxTransactionManager {
 		this.validateExistingTransaction = validateExistingTransaction;
 	}
 
+	/**
+	 * Returns <code>true</code> if this transaction manager works with just one resource.
+	 */
 	public boolean isSingleResourceManager() {
 		return oneResourceManager;
 	}
-
+	/**
+	 * Specifies if transaction manager works with just one resource.
+	 */
 	public void setSingleResourceManager(boolean oneResourceManager) {
 		this.oneResourceManager = oneResourceManager;
 	}
