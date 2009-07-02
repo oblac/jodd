0d0121de5ef51f555f33050bede4c399e9a8cd14
Igor <igor@jodd.org>
2009-07-02 11:54:57 +0000

    3.0.4. dependency loosen up

---
+++ mod/jodd-wot/src/jodd/jtx/db/DbJtxResourceManager.java
@@ -0,0 +1,82 @@
+// Copyright (c) 2003-2009, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.jtx.db;
+
+import jodd.jtx.JtxResourceManager;
+import jodd.jtx.JtxTransactionMode;
+import jodd.jtx.JtxException;
+import jodd.db.connection.ConnectionProvider;
+import jodd.db.DbSession;
+import jodd.db.DbTransactionMode;
+
+/**
+ * Database {@link jodd.jtx.JtxResourceManager} manages life-cycle of {@link jodd.db.DbSession} resources.
+ * Also acts as an adapter of resource object (of any type) and JTX engine.
+ * <p>
+ * Transaction resources may be of any type. The only thing what is important is that resource <b>must</b> be
+ * aware of its transactional state - is it in no-transactional mode (i.e. auto-commit), or under the transaction.
+ */
+public class DbJtxResourceManager implements JtxResourceManager<DbSession> {
+
+	protected final ConnectionProvider connectionProvider;
+
+	/**
+	 * Creates resource manager.
+	 */
+	public DbJtxResourceManager(ConnectionProvider connectionProvider) {
+		this.connectionProvider = connectionProvider;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Class<DbSession> getResourceType() {
+		return DbSession.class;
+	}
+
+	// ---------------------------------------------------------------- resource manager
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public DbSession beginTransaction(JtxTransactionMode jtxMode) {
+		DbSession session = new DbSession(connectionProvider);
+		if (jtxMode.isTransactional()) {
+			session.beginTransaction(JtxDbUtil.convertToDbMode(jtxMode));
+		}
+		return session;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void commitTransaction(DbSession resource) {
+		if (resource.isTransactionActive()) {
+			resource.commitTransaction();
+		}
+		resource.closeSession();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void rollbackTransaction(DbSession resource) {
+		try {
+			if (resource.isTransactionActive()) {
+				resource.rollbackTransaction();
+			}
+		} catch (Exception ex) {
+			throw new JtxException(ex);
+		} finally {
+			resource.closeSession();
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void close() {
+		connectionProvider.close();
+	}
+
+}
---
+++ mod/jodd-wot/src/jodd/jtx/db/JtxDbUtil.java
@@ -0,0 +1,31 @@
+// Copyright (c) 2003-2009, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.jtx.db;
+
+import jodd.db.DbTransactionMode;
+import jodd.jtx.JtxTransactionMode;
+
+/**
+ * Helpers for jtx db.
+ */
+public class JtxDbUtil {
+
+	/**
+	 * Convert JTX transaction mode to DB transaction mode.
+	 */
+	public static DbTransactionMode convertToDbMode(JtxTransactionMode txMode) {
+		int isolation = -1;
+		switch (txMode.getIsolationLevel()) {
+			case ISOLATION_DEFAULT: isolation = DbTransactionMode.ISOLATION_DEFAULT; break;
+			case ISOLATION_NONE: isolation = DbTransactionMode.ISOLATION_NONE; break;
+			case ISOLATION_READ_COMMITTED: isolation = DbTransactionMode.ISOLATION_READ_COMMITTED; break;
+			case ISOLATION_READ_UNCOMMITTED: isolation = DbTransactionMode.ISOLATION_READ_UNCOMMITTED; break;
+			case ISOLATION_REPEATABLE_READ: isolation = DbTransactionMode.ISOLATION_REPEATABLE_READ; break;
+			case ISOLATION_SERIALIZABLE: isolation = DbTransactionMode.ISOLATION_SERIALIZABLE; break;
+		}
+		DbTransactionMode result = new DbTransactionMode();
+		result.setIsolation(isolation);
+		result.setReadOnly(txMode.isReadOnly());
+		return result;
+	}
+}
