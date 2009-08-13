616b2d1af4886081097ca8a1370ebf5c6c419b20
Igor <igor@jodd.org>
2009-08-13 21:37:04 +0000

    build slightly enhanced. new target added.

--- build.py
+++ build.py
@@ -52,11 +52,12 @@ project()
 project_task('build', 'jodd, jodd-wot, jodd-gfx')
 project_task('javadoc', 'jodd, jodd-wot')
 project_task('emma', 'jodd, jodd-wot')
-project_task('dist', 'jodd, jodd.src, jodd.doc, jodd-wot, jodd-wot.src, jodd-wot.doc')
+project_task('dist', 'jodd, jodd-wot')
+project_task2('all', 'dist', 'jodd, jodd.src, jodd.doc, jodd-wot, jodd-wot.src, jodd-wot.doc')
 project_task('findbugs', 'jodd, jodd-wot')
 project_clean()
 
-project_target('release', 'clean, build, javadoc, emma, findbugs, dist', 'creates full release')
+project_target('release', 'clean, build, javadoc, emma, findbugs, all', 'creates full release')
 
 
 pack('dist', 'jodd',     '', '''
@@ -92,25 +93,27 @@ pack('all',  'jodd-all-with-dependencies', 'pack-dist-srcdoc', '''
 project_help('''
 Module targets
 --------------
-build - builds all modules
-javadoc - generates javadocs
-dist - creates all distribution jars
-findbugs - generates findbugs report
-clean - cleans out folders
+build:	builds all modules
+javadoc:	generates javadocs
+dist:	creates distribution jars
+findbugs:	generates findbugs report
+clean:	cleans out folders
 
 
 Project targets
 ---------------
-clean: cleans all outputs
-build: compile all
-javadoc: generates javadoc
-emma: runs all tests
-findbugs: find bugs
-dist: builds jars
+clean: 	cleans all outputs
+build:	compile all
+javadoc:	generates javadoc
+emma:	runs all tests
+findbugs:	finds bugs
+dist:	builds distribution jars
+all:	builds all jars
+
 
 Pack
 ----
-pack-dist: pack just distribution jars
-pack-all: pack all
+pack-dist:	pack just distribution jars
+pack-all:	pack all
 ''')
 project_footer()
