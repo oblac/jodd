456a260f0e4de06e98bb2008b3553f4599360243
Igor <igor@jodd.org>
2009-10-07 13:53:42 +0000

    compile error fixed;)

--- mod/jodd/test/jodd/servlet/ServletUtilTest.java
+++ mod/jodd/test/jodd/servlet/ServletUtilTest.java
@@ -8,15 +8,15 @@ public class ServletUtilTest extends TestCase {
 
 	public void testPrepareParameters() {
 		String[] p = new String[] {"one", "", " three ", null, "five"};
-		p = ServletUtil.prepareParameters(p, true, false, false, false);
+		p = ServletUtil.prepareParameters(p, true, false, false);
 		assertEquals("three", p[2]);
 		assertNotNull(p[1]);
 		assertNull(p[3]);
-		p = ServletUtil.prepareParameters(p, true, true, false, false);
+		p = ServletUtil.prepareParameters(p, true, true, false);
 		assertNull(p[1]);
 		assertNull(p[3]);
 
-		p = ServletUtil.prepareParameters(new String[] {"", null, "   "}, true, true, true, false);
+		p = ServletUtil.prepareParameters(new String[] {"", null, "   "}, true, true, true);
 		assertNull(p);
 	}
 }
