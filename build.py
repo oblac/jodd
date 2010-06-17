14e47740db3886b5d4f3b80e8fd8b2ec99039967
Igor <igor@jodd.org>
2010-06-17 21:55:15 +0000

    welcome maven

--- build.py
+++ build.py
@@ -2,8 +2,8 @@
 # settings
 prjName = 'Jodd'
 prjId = 'jodd'
-prjDescription = 'Jodd - generic purpose open-source Java library and frameworks.'
-prjVersion = '3.0.10'
+prjDescription = 'Jodd - open-source Java utility library and web application frameworks.'
+prjVersion = '3.1.0'
 
 # vars
 copyright = 'Copyright &#169; 2003-2010 Jodd Team'
@@ -27,6 +27,8 @@ module_do_doc('production')
 module_do_test('jodd.TestJodd')
 module_do_findbugs()
 module_dist(moduleName, 'jodd.Jodd')
+module_dist_sources(moduleName, 'production')
+module_dist_javadoc(moduleName, 'production')
 
 module('jodd-wot')
 module_compile('production', 'jdk5', '>jodd.production, servlets, asm, slf4j')
@@ -37,6 +39,9 @@ module_do_doc('production')
 module_do_test('jodd.TestJoddWot')
 module_do_findbugs()
 module_dist(moduleName, 'jodd.JoddWot')
+module_dist_sources(moduleName, 'production')
+module_dist_javadoc(moduleName, 'production')
+
 
 module('jodd-joy')
 module_compile('production', 'jdk5', '>jodd.production, >jodd-wot.production, servlets, slf4j')
@@ -53,6 +58,7 @@ project_task('javadoc', 'build, .jodd, .jodd-wot')
 project_task('emma', 'build, .jodd, .jodd-wot')
 project_task('findbugs', 'build, .jodd, .jodd-wot')
 project_task('dist', 'build, .jodd, .jodd-wot, .jodd-joy')
+project_task('dist-all', 'dist, dist-sources.jodd, dist-javadoc.jodd, dist-sources.jodd-wot, dist-javadoc.jodd-wot')
 project_clean()
 
 project_target('release', 'clean, build, javadoc, emma, findbugs, dist', 'creates full release')
@@ -62,6 +68,13 @@ pack_dist = '''
 	${jodd.jar}
 	${jodd-wot.jar}
 	file_id.diz
+'''
+pack_maven = pack_dist + '''
+	${jodd-sources.jar}
+	${jodd-javadoc.jar}
+	${jodd-wot-sources.jar}
+	${jodd-wot-javadoc.jar}
+	pom/**
 '''
 pack_src = pack_dist + '''
 	${jodd.production.src.dir}/**
@@ -77,8 +90,9 @@ pack_all = pack_src + '''
 	lib/**
 '''
 pack('dist', 'jodd', 'dist', pack_dist, '')
+pack('mvn',  'jodd-mvn', 'dist', pack_maven, '')
 pack('src',  'jodd-all', 'pack-dist', pack_src, '')
-pack('all',  'jodd-all-with-dependencies', 'pack-src', pack_all, 'lib/oracle/*')
+pack('all',  'jodd-all-with-dependencies', 'pack-src, pack-mvn', pack_all, 'lib/oracle/*')
 
 
 project_help('''
@@ -93,17 +107,19 @@ clean:	cleans out folders
 
 Project targets
 ---------------
-clean: 	cleans all outputs
+clean:	cleans all outputs
 build:	compile all
 javadoc:	generates javadoc
 emma:	runs all tests
 findbugs:	finds bugs
 dist:	builds distribution jars
+dist-all:	build all distribution jars
 
 
 Pack
 ----
 pack-dist:	pack just distribution jars
+pack-mvn:	pack distribution, sources and javadoc jars
 pack-src:	pack sources and documents
 pack-all:	pack all
 ''')
--- maven_deploy.bat
+++
@@ -1,2 +0,0 @@
-call mvn deploy:deploy-file -Durl=file:artifacts -Dfile=dist/jodd-3.0.4.jar     -DpomFile=mvn/jodd.pom
-call mvn deploy:deploy-file -Durl=file:artifacts -Dfile=dist/jodd-wot-3.0.4.jar -DpomFile=mvn/jodd-wot.pom
