f3d44f162c45ccf709d8753c9960c1eb9e854c70
Igor <igor@jodd.org>
2010-09-02 11:56:35 +0000

    utilities for iterating to list or set

--- mod/jodd/src/jodd/util/CollectionUtil.java
+++ mod/jodd/src/jodd/util/CollectionUtil.java
@@ -4,20 +4,47 @@ package jodd.util;
 
 import jodd.util.collection.EnumerationIterator;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Collection utilities.
  */
 public class CollectionUtil {
 
-
 	/**
 	 * Adapts an enumeration to an iterator.
 	 */
-	public static Iterator toIterator(Enumeration enumeration) {
-		return new EnumerationIterator(enumeration);
+	public static <E> Iterator<E> toIterator(Enumeration<E> enumeration) {
+		return new EnumerationIterator<E>(enumeration);
+	}
+
+	/**
+	 * Iterate elements to a list.
+	 * Returns an empty list if there are no elements to iterate.
+	 */
+	public static <E> List<E> iterateToList(Iterator<E> iterator) {
+		List<E> list = new ArrayList<E>();
+		while (iterator.hasNext()) {
+			list.add(iterator.next());
+		}
+		return list;
+	}
+
+	/**
+	 * Iterate elements to a set.
+	 * Returns an empty set if there are no elements to iterate.
+	 */
+	public static <E> Set<E> iterateToSet(Iterator<E> iterator) {
+		Set<E> set = new HashSet<E>();
+		while (iterator.hasNext()) {
+			set.add(iterator.next());
+		}
+		return set;
 	}
 
 }
