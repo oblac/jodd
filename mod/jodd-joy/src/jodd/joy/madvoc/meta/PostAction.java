3b4e69dbdf60f42603c84ce4216d4fc09c1b8aeb
Igor <igor@jodd.org>
2011-02-08 14:07:56 +0000

    custom annotation and post action

--- mod/jodd-joy/src/jodd/joy/madvoc/meta/PostAction.java
+++ mod/jodd-joy/src/jodd/joy/madvoc/meta/PostAction.java
@@ -1,6 +1,8 @@
 // Copyright (c) 2003-2011, Jodd Team (jodd.org). All Rights Reserved.
 
-package madvoc.meta;
+package jodd.joy.madvoc.meta;
+
+import jodd.joy.madvoc.action.AppAction;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -8,17 +10,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Post action annotation. Extension is set to '<b>do</b>' and method
+ * is set to '<b>POST</b>'.
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
-public @interface CustomUserAction {
+public @interface PostAction {
 
 	String value() default "";
 
-	String extension() default "custom";
+	String extension() default "do";
 
-//	String alias() default "";
+	String alias() default "";
 
-	String method() default "GET";
+	String method() default AppAction.METHOD_POST;
 
 }
