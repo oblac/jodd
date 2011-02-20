91f1e9f5b7efbfca7d4eaf6a3f099650a3f1c038
Igor <igor@jodd.org>
2011-02-20 07:53:54 +0000

    ---------- v3.2.5 ----------

--- build.py
+++ build.py
@@ -3,7 +3,7 @@
 prjName = 'Jodd'
 prjId = 'jodd'
 prjDescription = 'Jodd - open-source Java utility library and web application frameworks.'
-prjVersion = '3.2.5'
+prjVersion = '3.2.6'
 
 # vars
 copyright = 'Copyright &#169; 2003-2011 Jodd Team'
