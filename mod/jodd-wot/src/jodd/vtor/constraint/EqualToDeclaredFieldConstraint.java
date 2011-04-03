fd97afde54acdd545509d49381207707022425b3
Igor <igor@jodd.org>
2011-04-03 20:55:09 +0000

    vtor: equal to decalred fields

---
+++ mod/jodd-wot/src/jodd/vtor/constraint/EqualToDeclaredFieldConstraint.java
@@ -0,0 +1,59 @@
+// Copyright (c) 2003-2011, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.vtor.constraint;
+
+import jodd.bean.BeanException;
+import jodd.bean.BeanUtil;
+import jodd.vtor.ValidationConstraint;
+import jodd.vtor.ValidationConstraintContext;
+import jodd.vtor.VtorException;
+
+public class EqualToDeclaredFieldConstraint implements ValidationConstraint<EqualToDeclaredField> {
+
+	public EqualToDeclaredFieldConstraint() {
+	}
+
+	public EqualToDeclaredFieldConstraint(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	// ---------------------------------------------------------------- properties
+
+	protected String fieldName;
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	// ---------------------------------------------------------------- configure
+
+	public void configure(EqualToDeclaredField annotation) {
+		this.fieldName = annotation.value();
+	}
+
+	// ---------------------------------------------------------------- valid
+
+	public boolean isValid(ValidationConstraintContext vcc, Object value) {
+		return validate(vcc.getTarget(), value, fieldName);
+	}
+
+	public static boolean validate(Object target, Object value, String fieldName) {
+		if (value == null) {
+			return true;
+		}
+		Object valueToCompare;
+		try {
+			valueToCompare = BeanUtil.getDeclaredProperty(target, fieldName);
+		} catch (BeanException bex) {
+			throw new VtorException("Unable to read value to compare: '" + fieldName + "'.", bex);
+		}
+		if (valueToCompare == null) {
+			return false;
+		}
+		return value.equals(valueToCompare);
+	}
+}
