9eca491dfe203a9331d27327493f0b5816ff544c
Igor <igor@jodd.org>
2011-05-01 14:30:35 +0000

    Minor net updates

--- mod/jodd/src/jodd/util/NetUtil.java
+++ mod/jodd/src/jodd/util/NetUtil.java
@@ -16,73 +16,133 @@ import java.net.UnknownHostException;
  */
 public class NetUtil {
 
+	public static final String LOCAL_HOST = "localhost";
+	public static final String LOCAL_IP = "127.0.0.1";
+	public static final String DEFAULT_MASK = "255.255.255.0";
+	public static final int INT_VALUE_127_0_0_1 = 0x7f000001;
+
+
 	/**
-	 * Resolves hostname and returns ip address as a string.
+	 * Resolves IP address from a hostname.
 	 */
-	public static String resolveHost(String hostname) {
+	public static String resolveIpAddress(String hostname) {
 		try {
-			InetAddress addr = Inet4Address.getByName(hostname);
-			byte[] ipAddr = addr.getAddress();
-			StringBuilder ipAddrStr = new StringBuilder(15);
-			for (int i = 0; i < ipAddr.length; i++) {
-				if (i > 0) {
-					ipAddrStr.append('.');
-				}
-				ipAddrStr.append(ipAddr[i] & 0xFF);
+			InetAddress netAddress;
+
+			if (hostname == null || hostname.equalsIgnoreCase(LOCAL_HOST)) {
+				netAddress = InetAddress.getLocalHost();
+			} else {
+				netAddress = Inet4Address.getByName(hostname);
 			}
-			return ipAddrStr.toString();
-		} catch (UnknownHostException uhex) {
+			return netAddress.getHostAddress();
+		} catch (UnknownHostException ignore) {
 			return null;
 		}
 	}
 
 	/**
-	 * Resolves string ip address and returns host name as a string.
+	 * Returns IP address as integer.
 	 */
-	public static String resolveIp(String ip) {
-		try {
-			InetAddress addr = InetAddress.getByName(ip);
-			return addr.getHostName();
-		} catch (UnknownHostException uhex) {
-			return null;
+	public static int getIpAsInt(String ipAddress) {
+		int ipIntValue = 0;
+		String tokens[] = StringUtil.splitc(ipAddress, '.');
+		for (String token : tokens) {
+			if (ipIntValue > 0) {
+				ipIntValue <<= 8;
+			}
+			ipIntValue += Integer.parseInt(token);
 		}
+		return ipIntValue;
+	}
+
+	public static int getMaskAsInt(String mask) {
+		if (!validateHostIp(mask)) {
+			mask = DEFAULT_MASK;
+		}
+		return getIpAsInt(mask);
+	}
+
+	public static boolean isSocketAccessAllowed(int localIp, int socketIp, int mask) {
+		boolean _retVal = false;
+
+		if (socketIp == INT_VALUE_127_0_0_1 || (localIp & mask) == (socketIp & mask)) {
+			_retVal = true;
+		}
+		return _retVal;
 	}
 
 	/**
-	 * Resolves ip address and returns host name as a string.
+	 * Validates IP address given as a string.
 	 */
-	public static String resolveIp(byte[] ip) {
+	public static boolean validateHostIp(String host) {
+		boolean retVal = false;
+		if (host == null) {
+			return retVal;
+		}
+
+		int hitDots = 0;
+		char data[] = host.toCharArray();
+		for (int i = 0; i < data.length; i++) {
+			char c = data[i];
+			int b = 0;
+			do {
+				if (c < '0' || c > '9') {
+					return false;
+				}
+				b = (b * 10 + c) - 48;
+				if (++i >= data.length) {
+					break;
+				}
+				c = data[i];
+			} while (c != '.');
+
+			if (b > 255) {
+				return false;
+			}
+			hitDots++;
+		}
+
+		if (hitDots != 4) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Resolves host name from IP address bytes.
+	 */
+	public static String resolveHostName(byte[] ip) {
 		try {
-			InetAddress addr = InetAddress.getByAddress(ip);
-			return addr.getHostName();
-		} catch (UnknownHostException uhex) {
+			InetAddress address = InetAddress.getByAddress(ip);
+			return address.getHostName();
+		} catch (UnknownHostException ignore) {
 			return null;
 		}
 	}
 
 	// ---------------------------------------------------------------- download
-	
+
 	/**
 	 * Downloads resource as byte array.
 	 */
 	public static byte[] downloadBytes(String url) throws IOException {
-		HttpURLConnection conection = (HttpURLConnection)((new URL(url).openConnection()));
-		return StreamUtil.readBytes(conection.getInputStream());
+		HttpURLConnection connection = (HttpURLConnection) ((new URL(url).openConnection()));
+		return StreamUtil.readBytes(connection.getInputStream());
 	}
 
 	/**
 	 * Downloads resource as String.
 	 */
 	public static String downloadString(String url, String encoding) throws IOException {
-		HttpURLConnection conection = (HttpURLConnection)((new URL(url).openConnection()));
-		return new String(StreamUtil.readChars(conection.getInputStream(), encoding));
+		HttpURLConnection connection = (HttpURLConnection) ((new URL(url).openConnection()));
+		return new String(StreamUtil.readChars(connection.getInputStream(), encoding));
 	}
 
 	/**
 	 * Downloads resource as String.
 	 */
 	public static String downloadString(String url) throws IOException {
-		HttpURLConnection conection = (HttpURLConnection)((new URL(url).openConnection()));
-		return new String(StreamUtil.readChars(conection.getInputStream()));
+		HttpURLConnection connection = (HttpURLConnection) ((new URL(url).openConnection()));
+		return new String(StreamUtil.readChars(connection.getInputStream()));
 	}
 }
