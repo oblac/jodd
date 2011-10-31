e7ffee86e3c040684e9d4b180f6c0355d3e704a9
Igor <igor@jodd.org>
2011-10-31 22:41:00 +0000

    AsmUtil enhanced

--- mod/jodd-wot/src/jodd/asm/AsmUtil.java
+++ mod/jodd-wot/src/jodd/asm/AsmUtil.java
@@ -3,27 +3,106 @@
 package jodd.asm;
 
 import jodd.mutable.MutableInteger;
-import jodd.proxetta.ProxettaException;
+import jodd.util.ClassLoaderUtil;
 
 /**
  * Generic ASM utils.
  */
 public class AsmUtil {
 
-	// ---------------------------------------------------------------- converters
+	private static final String INVALID_BASE_TYPE = "Invalid base type: ";
+	private static final String INVALID_TYPE_DESCRIPTION = "Invalid type description: ";
+
+	// ---------------------------------------------------------------- class relates
+
+	/**
+	 * Converts bytecode-like description to java class name that can be loaded
+	 * with a classloader. Uses less-known feature of class loaders for loading
+	 * array classes. For base types returns the one-letter string that can be used
+	 * with {@link #loadBaseTypeClass(String)}.
+	 *
+	 * @see #typedescToSignature(String, jodd.mutable.MutableInteger)
+	 */
+	public static String typedesc2ClassName(String desc) {
+		String className = desc;
+		switch (desc.charAt(0)) {
+			case 'B':
+			case 'C':
+			case 'D':
+			case 'F':
+			case 'I':
+			case 'J':
+			case 'S':
+			case 'Z':
+			case 'V':
+				if (desc.length() != 1) {
+					throw new IllegalArgumentException(INVALID_BASE_TYPE + desc);
+				}
+				break;
+			case 'L': className = className.substring(1, className.length() - 1);
+			case '[':
+				// uses less-known feature of class loaders for loading array types
+				// using bytecode-like signatures.
+				className = className.replace('/', '.');
+				break;
+			default: throw new IllegalArgumentException(INVALID_TYPE_DESCRIPTION + desc);
+		}
+
+		return className;
+	}
 
 	/**
 	 * Converts type reference to java-name.
 	 */
 	public static String typeref2Name(String desc) {
 		if (desc.charAt(0) != AsmConst.TYPE_REFERENCE) {
-			throw new ProxettaException("Invalid type description/reference.");
+			throw new IllegalArgumentException(INVALID_TYPE_DESCRIPTION + desc);
 		}
 		String name = desc.substring(1, desc.length() - 1);
 		return name.replace('/', '.');
 	}
 
+	/**
+	 * Loads class defined by bytecode-like descriptor,
+	 */
+	public static Class loadClass(String desc) throws ClassNotFoundException {
+		String className = typedesc2ClassName(desc);
+		if (className.length() == 1) {
+			return loadBaseTypeClass(className);
+		}
+		return ClassLoaderUtil.loadClass(className, AsmUtil.class);
+	}
+
+	/**
+	 * Loads base class type.
+	 */
+	public static Class loadBaseTypeClass(String desc) throws ClassNotFoundException {
+		if (desc.length() != 1) {
+			throw new ClassNotFoundException(INVALID_BASE_TYPE + desc);
+		}
+		switch (desc.charAt(0)) {
+			case 'B': return byte.class;
+			case 'C': return char.class;
+			case 'D': return double.class;
+			case 'F': return float.class;
+			case 'I': return int.class;
+			case 'J': return long.class;
+			case 'S': return short.class;
+			case 'Z': return boolean.class;
+			case 'V': return void.class;
+			default: throw new ClassNotFoundException(INVALID_BASE_TYPE + desc);
+		}
+	}
+
 	// ---------------------------------------------------------------- description
+
+	/**
+	 * Returns java-like signature of a bytecode-like description.
+	 * @see #typedescToSignature(String, jodd.mutable.MutableInteger)
+	 */
+	public static String typedescToSignature(String desc) {
+		return typedescToSignature(desc, new MutableInteger());
+	}
 
 	/**
 	 * Returns java-like signature of a bytecode-like description.
@@ -74,7 +153,7 @@ public class AsmUtil {
 			case 'L':
 				int index = desc.indexOf(';', fromIndex);
 				if (index < 0) {
-					throw new ProxettaException("Invalid type description: " + desc);
+					throw new IllegalArgumentException(INVALID_TYPE_DESCRIPTION + desc);
 				}
 				from.setValue(index + 1);
 				String str = desc.substring(fromIndex + 1, index);
@@ -91,16 +170,8 @@ public class AsmUtil {
 				String type = typedescToSignature(desc, from);	// the rest of the string denotes a `<field_type>'
 				return type + brackets;
 
-			default: throw new ProxettaException("Invalid type description: " + desc);
+			default: throw new IllegalArgumentException(INVALID_TYPE_DESCRIPTION + desc);
 		}
-	}
-
-	/**
-	 * Returns java-like signature of a bytecode-like description.
-	 * @see #typedescToSignature(String, jodd.mutable.MutableInteger)
-	 */
-	public static String typedescToSignature(String desc) {
-		return typedescToSignature(desc, new MutableInteger());
 	}
 
 }
