fc99eebda7c7390bf3033c46be09c5e8bb57b907
Igor <igor@jodd.org>
2011-12-19 12:56:30 +0000

    fixed package-info

--- mod/jodd/src/jodd/package-info.java
+++ mod/jodd/src/jodd/package-info.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2003-2012, Jodd Team (jodd.org). All Rights Reserved.
 
 /**
- * Jodd = tools + ioc + mvc + db + aop + tx < 800KB
+ * Jodd = tools + ioc + mvc + db + aop + tx + html < 1.1M
  */
 package jodd;
