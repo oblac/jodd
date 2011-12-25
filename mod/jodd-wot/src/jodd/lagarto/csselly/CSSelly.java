0c10ecdc1d2cef4510f22ba5a56c6630267bf04d
Igor <igor@jodd.org>
2011-12-25 21:41:11 +0000

    javadoc formatting

--- mod/jodd-wot/src/jodd/lagarto/csselly/CSSelly.java
+++ mod/jodd-wot/src/jodd/lagarto/csselly/CSSelly.java
@@ -12,8 +12,7 @@ import java.util.List;
  * CSS selector parser. Works with one query, i.e. does not support groups
  * of selectors (selectors separated by a comma). To parse selectors group,
  * manually split the group query into single queries and parse each.
- *
- * @see http://www.w3.org/TR/css3-selectors/#w3cselgrammar
+ * See: http://www.w3.org/TR/css3-selectors/#w3cselgrammar
  */
 public class CSSelly {
 
