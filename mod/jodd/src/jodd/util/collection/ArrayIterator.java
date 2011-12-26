ca8e522e6d7037e23fe6a80370382827b75ba811
Igor <igor@jodd.org>
2011-12-26 12:24:08 +0000

    source formatting

--- mod/jodd/src/jodd/util/collection/ArrayIterator.java
+++ mod/jodd/src/jodd/util/collection/ArrayIterator.java
@@ -12,33 +12,33 @@ import java.util.NoSuchElementException;
  */
 public class ArrayIterator implements Iterator, Serializable {
 	
-	private Object mArray[];
-	private int mCurrentElement;
-	private int mArrayLength;
-	private int mOffset;	
+	private Object array[];
+	private int currentIndex;
+	private int length;
+	private int offset;
 
 	public ArrayIterator(Object aobj[]) {
-		mArray = aobj;
-		mCurrentElement = 0;
-		mOffset = 0;
-		mArrayLength = mArray.length;
+		array = aobj;
+		currentIndex = 0;
+		offset = 0;
+		length = array.length;
 	}
 
-	public ArrayIterator(Object aobj[], int i, int j) {
-		mArray = aobj;
-		mCurrentElement = i;
-		mOffset = i;
-		mArrayLength = (j - i) + 1;
+	public ArrayIterator(Object aobj[], int offset, int len) {
+		array = aobj;
+		currentIndex = offset;
+		this.offset = offset;
+		length = len;
 	}
 
 	public boolean hasNext() {
-		return mCurrentElement < mArrayLength + mOffset;
+		return currentIndex < length + offset;
 	}
 
 	public Object next() throws NoSuchElementException {
 		try {
-			mCurrentElement++;
-			return mArray[mCurrentElement - 1];
+			currentIndex++;
+			return array[currentIndex - 1];
 		} catch (ArrayIndexOutOfBoundsException ignored) {
 			throw new NoSuchElementException();
 		}
