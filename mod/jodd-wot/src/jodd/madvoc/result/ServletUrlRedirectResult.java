ec5ac08cb74e5373817ec2310b0dc62b2dcb1c7e
Igor <igor@jodd.org>
2012-01-02 08:40:26 +0000

    add 'url:' madvoc result

---
+++ mod/jodd-wot/src/jodd/madvoc/result/ServletUrlRedirectResult.java
@@ -0,0 +1,30 @@
+package jodd.madvoc.result;
+
+import jodd.bean.BeanTemplateParser;
+import jodd.madvoc.ActionRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Simply sends redirect to an external location.
+ */
+public class ServletUrlRedirectResult extends ActionResult  {
+
+	public static final String NAME = "url";
+
+	private static BeanTemplateParser beanTemplateParser = new BeanTemplateParser();
+
+	public ServletUrlRedirectResult() {
+		super(NAME);
+	}
+
+	/**
+	 * Redirects to the external location.
+	 */
+	@Override
+	public void render(ActionRequest actionRequest, Object resultObject, String resultValue, String resultPath) throws Exception {
+		resultValue = beanTemplateParser.parse(resultValue, actionRequest.getAction());
+		HttpServletResponse response = actionRequest.getHttpServletResponse();
+		response.sendRedirect(resultValue);
+	}
+
+}
