1306c822ce8761c0e19c02a35887b884a9694dd5
Igor <igor@jodd.org>
2012-02-11 23:49:04 +0000

    javadoc

--- mod/jodd/src/jodd/typeconverter/TypeConverter.java
+++ mod/jodd/src/jodd/typeconverter/TypeConverter.java
@@ -5,22 +5,20 @@ package jodd.typeconverter;
 /**
  * Object converter interface.
  *
- * @see jodd.typeconverter.TypeConverterManager
+ * @see jodd.typeconverter.TypeConverterManagerBean
  */
 public interface TypeConverter<T> {
 
 	/**
 	 * Converts object received as parameter into object of another class.
-	 * For example, in a IntegerConverter implementation of this interface,
-	 * this method should convert all objects into Integer object. Conversion has
-	 * to be done correctly, with investigation of given object.
-	 * <p>
+	 * For example, an <code>Integer</code> converter tries to convert given objects
+	 * into target <code>Integer</code> object. Converters should try all reasonable
+	 * ways of conversion into target object, depending on target type.
 	 *
-	 * If conversion is not possible {@link TypeConversionException} should be thrown.
+	 * @param value object to convert from
 	 *
-	 * @param value    object to convert from
-	 *
-	 * @return resulting object
+	 * @return resulting object converted to target type
+	 * @throws TypeConversionException if conversion fails
 	 */
 	T convert(Object value);
 
