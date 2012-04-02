a01d157329d2ee5ce5678a94b113c437c14fca40
Igor <igor@jodd.org>
2012-04-02 19:43:34 +0000

    doc

--- mod/jodd/src/jodd/servlet/filter/package-info.java
+++ mod/jodd/src/jodd/servlet/filter/package-info.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2003-2012, Jodd Team (jodd.org). All Rights Reserved.
 
 /**
- * Just few common JSP filters.
+ * Just few common servlet filters and response wrappers.
  */
 package jodd.servlet.filter;
