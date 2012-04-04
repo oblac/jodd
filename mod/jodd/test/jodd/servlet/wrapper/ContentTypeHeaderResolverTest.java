feb20dbf49719214879965273721a63f8dc22e97
Igor <igor@jodd.org>
2012-04-04 09:35:36 +0000

    test for cthr

--- mod/jodd/test/jodd/servlet/wrapper/ContentTypeHeaderResolverTest.java
+++ mod/jodd/test/jodd/servlet/wrapper/ContentTypeHeaderResolverTest.java
@@ -1,18 +1,18 @@
 // Copyright (c) 2003-2012, Jodd Team (jodd.org). All Rights Reserved.
 
-package jodd.decora;
+package jodd.servlet.wrapper;
 
 import junit.framework.TestCase;
 
-public class TestContentTypeHeaderResolver extends TestCase {
+public class ContentTypeHeaderResolverTest extends TestCase {
 
 	public void testResolver() {
 		ContentTypeHeaderResolver cthr = new ContentTypeHeaderResolver("text/html; charset=utf-8");
-		assertEquals("text/html", cthr.getType());
+		assertEquals("text/html", cthr.getMimeType());
 		assertEquals("utf-8", cthr.getEncoding());
 
 		cthr = new ContentTypeHeaderResolver("text/html");
-		assertEquals("text/html", cthr.getType());
+		assertEquals("text/html", cthr.getMimeType());
 		assertNull(cthr.getEncoding());
 	}
 }
