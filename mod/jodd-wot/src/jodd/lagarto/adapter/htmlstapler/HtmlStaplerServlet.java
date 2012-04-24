df36e7563022329085ae4b4565ea6eb5875240f8
Igor <igor@jodd.org>
2012-04-24 21:29:34 +0000

    better gzip filter, added wildcard feature

--- mod/jodd-wot/src/jodd/lagarto/adapter/htmlstapler/HtmlStaplerServlet.java
+++ mod/jodd-wot/src/jodd/lagarto/adapter/htmlstapler/HtmlStaplerServlet.java
@@ -31,13 +31,13 @@ public class HtmlStaplerServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String bundleId = req.getParameter("id");
 		File file = bundlesManager.lookupBundleFile(bundleId);
-		writeBundleFile(resp, file);
+		sendBundleFile(resp, file);
 	}
 
 	/**
 	 * Outputs bundle file to the response.
 	 */
-	protected void writeBundleFile(HttpServletResponse resp, File bundleFile) throws IOException {
+	protected void sendBundleFile(HttpServletResponse resp, File bundleFile) throws IOException {
 		OutputStream out = resp.getOutputStream();
 		StreamUtil.copy(new FileInputStream(bundleFile), out);
 	}
--- mod/jodd/src/jodd/servlet/filter/GzipFilter.java
+++ mod/jodd/src/jodd/servlet/filter/GzipFilter.java
@@ -2,8 +2,13 @@
 
 package jodd.servlet.filter;
 
+import jodd.servlet.ServletUtil;
+import jodd.typeconverter.Convert;
+import jodd.typeconverter.TypeConversionException;
+import jodd.util.StringUtil;
+import jodd.util.Wildcard;
+
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -15,63 +20,52 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Compresses output with gzip, for browsers that supports it.<p>
- *
- * Configuration of this filter is based on the following initialization
- * parameters:
+ * Compresses output with gzip, for browsers that supports it.
+ * <p>
+ * Configuration is based on the following initialization parameters:
  *
  * <ul>
- * <li><strong>threshold</strong> - min number of bytes for compressing
+ * <li><b>threshold</b> - min number of bytes for compressing
  * or 0 for no compression at all.</li>
  *
- * <li><strong>match</strong> - comma separated string patterns to be found
+ * <li><b>match</b> - comma separated string patterns to be found
  * in uri for using gzip. Only uri's that have these patterns inside will use
- * gzip.</li>
+ * gzip. Use '*' for applying gzip on all resources (ignoring the wildcards value)</li>
  *
- * <li><strong>excludes</strong> - comma separated string patterns to be excluded
- * if found in uri for using gzip. It is applied only on matched uris.</li>
+ * <li><b>excludes</b> - comma separated string patterns to be excluded
+ * if found in uri for using gzip. It is applied only on <b>matched</b> uris.</li>
+ *
+ * <li><b>wildcards</b> - boolean that specifies wildcard matching for string patterns.
+ * by default <code>false</code>.</li>
  *
  * </ul>
- *
- * <p>
- * This filter has been build and extend from Tomcats example.
- * <p>
- *
- * Technical notes: i have found that this is the only way how gzip filter
- * can be build. The first idea that comes to mind is to wrap response to
- * some character-based wrapper, and then to gzip it to the output. This
- * works fine except when forwarding is used: forwarded page is gzipped, but
- * response header is no more there! I have not an idea how to fix this
- * except from this approach presented in Tomcat.
  */
 public class GzipFilter implements Filter {
 	
-	private FilterConfig config;
-
-	protected FilterConfig getFilterConfig() {
-		return (config);
-	}
-
 	/**
-	 * If browser supports gzip, set the Content-Encoding response header and
+	 * If browser supports gzip, sets the Content-Encoding response header and
 	 * invoke resource with a wrapped response that collects all the output.
-	 * Extract the output and write it into a gzipped byte array. Finally, write
+	 * Extracts the output and write it into a gzipped byte array. Finally, write
 	 * that array to the client's output stream.
-	 *
-	 * If browser does not support gzip, invoke resource normally.
-	 *
-	 * @exception ServletException
-	 * @exception IOException
+	 * <p>
+	 * If browser does not support gzip, invokes resource normally.
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
-		if ( (threshold == 0) || (isGzipSupported(req) == false) || (isGzipEligible(req) == false) ) {
+
+		if (
+				(threshold == 0) ||
+				(ServletUtil.isGzipSupported(req) == false) ||
+				(isGzipEligible(req) == false)
+		) {
 			chain.doFilter(request, response);
 			return;
 		}
+
 		GzipResponseWrapper wrappedResponse = new GzipResponseWrapper(res);
 		wrappedResponse.setCompressionThreshold(threshold);
+
 		try {
 			chain.doFilter(request, wrappedResponse);
 		} finally {
@@ -80,117 +74,104 @@ public class GzipFilter implements Filter {
 	}
 
 	/**
-	 * Comma separated string patterns to be found in the request URI
-	 */
-	protected String uriMatch;
-	
-	/**
-	 * Comma separated string patterns to be excluded in the request URI if
-	 * founded by match.
-	 */
-	protected String uriExclude;
-
-	/**
 	 * Minimal threshold.
 	 */
-	private int minThreshold = 128;
+	protected int minThreshold = 128;
 
 	/**
 	 * The threshold number to compress, (0 == no compression).
 	 */
     protected int threshold;
 
-	private String[] extensions;
-	private String[] excludes;
+	protected String[] matches;
+	protected String[] excludes;
+	protected boolean wildcards;
 
 	/**
 	 * Filter initialization.
-	 *
-	 * @exception ServletException
 	 */
 	public void init(FilterConfig config) throws ServletException {
-		this.config = config;
-		extensions = null;
+
+		try {
+			wildcards = Convert.toBooleanValue(config.getInitParameter("wildcards"), false);
+		} catch (TypeConversionException ignore) {
+			wildcards = false;
+		}
 
 		// min size
 		try {
-			threshold = Integer.parseInt(config.getInitParameter("threshold"));
-		} catch (NumberFormatException nfe) {
+			threshold = Convert.toIntValue(config.getInitParameter("threshold"), 0);
+		} catch (TypeConversionException ignore) {
 			threshold = 0;
 		}
+
 		if (threshold < minThreshold) {
 			threshold = 0;
 		}
 
 		// match string
-		uriMatch = config.getInitParameter("match");
+		String uriMatch = config.getInitParameter("match");
+
 		if ((uriMatch != null) && (uriMatch.equals("*") == false)) {
-			StringTokenizer st = new StringTokenizer(uriMatch, ",");
-			int i = st.countTokens();
-			if (i >= 1) {
-				extensions = new String[i];
-				i = 0;
-				while (st.hasMoreTokens()) {
-					extensions[i] = st.nextToken().trim();
-					i++;
-				}
+			matches = StringUtil.splitc(uriMatch, ',');
+			for (int i = 0; i < matches.length; i++) {
+				matches[i] = matches[i].trim();
 			}
 		}
+
 		// exclude string
-		uriExclude = config.getInitParameter("exclude");
+		String uriExclude = config.getInitParameter("exclude");
+
 		if (uriExclude != null) {
-			StringTokenizer st = new StringTokenizer(uriExclude, ",");
-			int i = st.countTokens();
-			if (i >= 1) {
-				excludes = new String[i];
-				i = 0;
-				while (st.hasMoreTokens()) {
-					excludes[i] = st.nextToken().trim();
-					i++;
-				}
+			excludes = StringUtil.splitc(uriExclude, ',');
+			for (int i = 0; i < excludes.length; i++) {
+				excludes[i] = excludes[i].trim();
 			}
 		}
 	}
 
 	public void destroy() {
-		config = null;
-		extensions = null;
+		matches = null;
 		excludes = null;
-	}
-
-	private boolean isGzipSupported(HttpServletRequest req) {
-		String browserEncodings = req.getHeader("Accept-Encoding");
-		return (browserEncodings != null) && (browserEncodings.indexOf("gzip") != -1);
 	}
 
 	/**
 	 * Determine if uri is eligible for gzip-ing.
-	 *
-	 * @return true to gzip the uri, otherwise false
 	 */
 	private boolean isGzipEligible(HttpServletRequest req) {
 		String uri = req.getRequestURI();
 		if (uri == null) {
 			return false;
 		}
+
 		boolean result = false;
 		
-		if (extensions == null) {						// match=*
+		if (matches == null) {							// match=*
 			result = true;
 		} else {
-			for (String extension : extensions) {
-				if (uri.indexOf(extension) != -1) {
-					result = true;						// extension founded
-					break;
+			if (wildcards) {
+				result = Wildcard.matchOne(uri, matches) != -1;
+			} else {
+				for (String match : matches) {
+					if (uri.contains(match)) {
+						result = true;
+						break;
+					}
 				}
 			}
 		}
 
 		if ((result == true) && (excludes != null)) {
-			for (String exclude : excludes) {
-				if (uri.indexOf(exclude) != -1) {
-					result = false;						// excludes founded
-					break;
+			if (wildcards) {
+				if (Wildcard.matchOne(uri, excludes) != -1) {
+					result = false;
+				}
+			} else {
+				for (String exclude : excludes) {
+					if (uri.contains(exclude)) {
+						result = false;						// excludes founded
+						break;
+					}
 				}
 			}
 		}
