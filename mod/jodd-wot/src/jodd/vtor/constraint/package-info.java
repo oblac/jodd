53938dad6d05246b15b260a178b3df08ea30e054
Igor <igor@jodd.org>
2012-06-12 09:42:00 +0000

    javadoc

--- mod/jodd-wot/src/jodd/vtor/constraint/package-info.java
+++ mod/jodd-wot/src/jodd/vtor/constraint/package-info.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2003-2012, Jodd Team (jodd.org). All Rights Reserved.
 
 /**
- * <html><body>Various constraints and corresponding annotations. Most constraints has public static valid() method so
- * they can be used independently.</body></html>
+ * Various constraints and corresponding annotations. Most constraints has public static valid() method so
+ * they can be used independently.
  */
 package jodd.vtor.constraint;
--- mod/jodd-wot/src/jodd/vtor/package-info.java
+++ mod/jodd-wot/src/jodd/vtor/package-info.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2003-2012, Jodd Team (jodd.org). All Rights Reserved.
 
 /**
- * <html><body>Efficient validation tool.</body></html>
+ * Efficient validation tool.
  */
 package jodd.vtor;
