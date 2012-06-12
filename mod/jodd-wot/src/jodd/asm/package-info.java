b06f87c1f1617b2f3c55f02db9cf8707344647f2
Igor <igor@jodd.org>
2012-06-12 09:39:18 +0000

    javadoc

--- mod/jodd-wot/src/jodd/asm/package-info.java
+++ mod/jodd-wot/src/jodd/asm/package-info.java
@@ -1,10 +1,6 @@
 // Copyright (c) 2003-2012, Jodd Team (jodd.org). All Rights Reserved.
 
 /**
- * <html>
- * <body>
  * Some common ASM classes, used by various Jodd-WOT frameworks.
- * </body>
- * </html>
  */
 package jodd.asm;
