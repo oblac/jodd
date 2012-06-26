4e585c65679d28180b5b0eea85e63cf84a04e778
Igor <igor@jodd.org>
2012-06-26 18:51:48 +0000

    fixed little bug and javadoc change

--- mod/jodd-wot/src/jodd/lagarto/htmlstapler/HtmlStaplerFilter.java
+++ mod/jodd-wot/src/jodd/lagarto/htmlstapler/HtmlStaplerFilter.java
@@ -29,15 +29,8 @@ import java.io.OutputStream;
 import static jodd.lagarto.htmlstapler.HtmlStaplerBundlesManager.Strategy;
 
 /**
- * HtmlStapler filter. Parameters:
- * <li>enabled - if the whole filter is enabled or not (<code>true</code> by default)
- * <li>stripHtml - if the strip html feature is turned on (<code>true</code> by default)
- * <li>resetOnStart - if bundles should be reset on server start (<code>true</code> by default)
- * <li>useGzip - if gzip should be used (<code>false</code> by default)
- * <li>cacheMaxAge - cache header max-age value (30 days by default), if set to 0 it will not be set.
- * <li>staplerStrategy - strategy (<code>RESOURCE_ONLY</code> by default)</li>
- *
- * Some additional settings can be applied on created
+ * HtmlStapler filter.
+ * Part of the parameters are here, the other part is in the
  * {@link #createBundleManager(javax.servlet.ServletContext, jodd.lagarto.htmlstapler.HtmlStaplerBundlesManager.Strategy) bundle manager}.
  */
 public class HtmlStaplerFilter extends SimpleLagartoServletFilter {
@@ -97,7 +90,7 @@ public class HtmlStaplerFilter extends SimpleLagartoServletFilter {
 			String value = filterConfig.getInitParameter(parameter);
 
 			if (value != null) {
-				BeanUtil.setProperty(target, parameter, value);
+				BeanUtil.setDeclaredProperty(target, parameter, value);
 			}
 		}
 	}
