03e5ec7426202d6613e0ae793606689fe0061aa6
Igor <igor@jodd.org>
2012-08-20 10:13:19 +0000

    removed sorting algorithms, as only one should rule :)

--- mod/jodd/src/jodd/util/sort/package-info.java
+++ mod/jodd/src/jodd/util/sort/package-info.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2003-2012, Jodd Team (jodd.org). All Rights Reserved.
 
 /**
- * Fastest sorting algoritms and implementations.
+ * Fastest sorting algorithms implementations.
  */
 package jodd.util.sort;
