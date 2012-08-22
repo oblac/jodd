4064122e1cbadb3e8971c74e30964447f5a1f42d
Igor <igor@jodd.org>
2012-08-22 23:44:52 +0000

    init moved to jodd

--- mod/jodd-joy/src/jodd/joy/jsp/JoddJoyJspFunctions.java
+++ mod/jodd-joy/src/jodd/joy/jsp/JoddJoyJspFunctions.java
@@ -2,28 +2,9 @@
 
 package jodd.joy.jsp;
 
-import jodd.servlet.PageContextThreadLocal;
-import jodd.servlet.ServletUtil;
-
-import javax.servlet.jsp.PageContext;
-
 /**
  * Some jsp functions.
  */
 public class JoddJoyJspFunctions {
 
-	private static final String CTX_VAR_NAME = "CTX";
-
-	/**
-	 * Performs page initialization. The following is done:
-	 * <ul>PageContextThreadLocal is set</ul>
-	 * <ul>CTX page attribute is set with context path value</ul>
-	 * <ul>CTX request attribute is set with context path value</ul>
-	 */
-	public static void initPage(PageContext pageContext) {
-		PageContextThreadLocal.set(pageContext);
-		String ctxPath = ServletUtil.getContextPath(pageContext);
-		pageContext.setAttribute(CTX_VAR_NAME, ctxPath);
-		pageContext.getRequest().setAttribute(CTX_VAR_NAME, ctxPath);
-	}
 }
