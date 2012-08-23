dc19727e6684d5f7fd2882d0243db302563e7416
Igor <igor@jodd.org>
2012-08-23 09:47:23 +0000

    repager

--- mod/jodd-joy/www/repager.js
+++ mod/jodd-joy/www/repager.js
@@ -5,8 +5,45 @@
 function RePager(pagerId) {
 	this.pagerId = pagerId;
 	this.page = 1;
+	this.sort = 0;
 	this.body = $('#pagerBody-' + pagerId);
 	this.form = $('form#pagerForm-' + pagerId);
+	this.table = $('table#pagerTable-' + pagerId);
+
+	var pager = this;
+
+	$('thead td.sort', this.table).click(function() {
+		var td = $(this);
+		var s = 0;
+
+		$('thead td.sort', pager.table).each(function(index, element) {
+			if (element != td[0]) {
+				$(element).removeClass('sort_asc').removeClass('sort_desc');
+			} else {
+				s = index + 1;
+			}
+		});
+
+		if (td.hasClass('sort_asc')) {
+			td.removeClass('sort_asc');
+			td.addClass('sort_desc');
+			s = -s;
+		} else {
+			td.addClass('sort_asc');
+			td.removeClass('sort_desc');
+		}
+		pager.sort = s;
+		pager.goto();
+	});
+
+	// initialize
+	$('thead td.sort', this.table).each(function(index, element) {
+		if ($(element).hasClass('sort_asc')) {
+			pager.sort = index + 1;
+		} else if ($(element).hasClass('sort_desc')) {
+			pager.sort = -(index + 1);
+		}
+	});
 
 	this.goto = function(p) {
 		if (!p) {
@@ -20,7 +57,8 @@ function RePager(pagerId) {
 		var options = {
 			data: {
 				'pageRequest.page': pager.page,
-				'pageRequest.pagerId' : pager.pagerId
+				'pageRequest.pagerId' : pager.pagerId,
+				'pageRequest.sort' : pager.sort
 			},
 			success: function(response) {
 				if (response.length == 0) {		// fix dialog escape bug
