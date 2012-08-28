18975b341122055dea2e6e61a7a5c6be8fbc3318
Igor <igor@jodd.org>
2012-08-28 12:57:08 +0000

    proxetta stripped down.

--- mod/jodd-wot/src/jodd/proxetta/Proxetta.java
+++ mod/jodd-wot/src/jodd/proxetta/Proxetta.java
@@ -2,13 +2,7 @@
 
 package jodd.proxetta;
 
-import java.io.InputStream;
-
-import jodd.log.Log;
 import jodd.proxetta.asm.ClassProcessor;
-import jodd.proxetta.impl.InvokeProxetta;
-import jodd.proxetta.impl.ProxyProxetta;
-import jodd.util.ClassLoaderUtil;
 
 /**
  * Proxetta creates dynamic proxy classes in the run-time.
@@ -30,95 +24,71 @@ import jodd.util.ClassLoaderUtil;
  * can be in the following forms:
  * <li> .Foo (starting with a dot) - proxy package name is equal to target package, just proxy simple class name is set.
  * <li> foo. (ending with a dot) - proxy package is set, proxy simple name is create from target simple class name.
- * <li> foo.Foo - full proxy class name is specified. 
+ * <li> foo.Foo - full proxy class name is specified.
  */
 public abstract class Proxetta {
 
-	private static final Log log = Log.getLogger(Proxetta.class);
-
-	/**
-	 * Specifies aspects for the target and creates Proxetta instance.
-	 */
-	public static Proxetta withAspects(ProxyAspect... aspects) {
-		return new ProxyProxetta(aspects);
-	}
-
-	/**
-	 * Specifies invoke replacement aspects and creates Proxetta instance.
-	 */
-	public static Proxetta withAspects(InvokeAspect... aspects) {
-		return new InvokeProxetta(aspects);
-	}
-
-	// ---------------------------------------------------------------- forced
+	// ---------------------------------------------------------------- properties
 
 	protected boolean forced;
+	protected ClassLoader classLoader;
+	protected boolean variableClassName;
+	protected String classNameSuffix;
 
 	/**
 	 * Specifies 'forced' mode. If <code>true</code>, new proxy class will be created even if there are no
 	 * matching pointcuts. If <code>false</code>, new proxy class will be created only if there is at least one
 	 * matching pointcut - otherwise, original class will be returned.
 	 */
-	public Proxetta forced(boolean forced) {
+	public void setForced(boolean forced) {
 		this.forced = forced;
-		return this;
 	}
 
-	// ---------------------------------------------------------------- class loader
+	public boolean isForced() {
+		return forced;
+	}
 
-	protected ClassLoader classLoader;
 
 	/**
-	 * Specifies classloaders for use.
+	 * Specifies classloaders for loading created classes..
 	 */
-	public Proxetta loadsWith(ClassLoader classLoader) {
+	public void setClassLoader(ClassLoader classLoader) {
 		this.classLoader = classLoader;
-		return this;
 	}
 
-	// ---------------------------------------------------------------- var name
+	public ClassLoader getClassLoader() {
+		return classLoader;
+	}
 
-	protected boolean variableClassName;
 
 	/**
-	 * Sets variable proxy class name so every time when new proxy class is created its name will be different,
-	 * so one classloader may load it without a problem.
+	 * Sets variable proxy class name so every time when new proxy class is created
+	 * its name will be different,so one classloader may load it without a problem.
+	 * Otherwise, sets constant proxy class name so each time created proxy
+	 * class will have the same name. Such class can be loaded only once by a classloader.
+	 * <p>
+	 * This prevents "<code>java.lang.LinkageError: duplicate class definition</code>" errors.
 	 */
-	public Proxetta variableClassName() {
-		variableClassName = true;
-		return this;
+	public void setVariableClassName(boolean variableClassName) {
+		this.variableClassName = variableClassName;
 	}
 
-	/**
-	 * Sets constant proxy class name so each time created proxy class will have the same name.
-	 * Such class can be loaded only once by a classloader.
-	 */
-	public Proxetta constantClassName() {
-		variableClassName = false;
-		return this;
+	public boolean isVariableClassName() {
+		return variableClassName;
 	}
 
-
-	// ---------------------------------------------------------------- suffix
-
-	protected String classNameSuffix;
 
 	/**
 	 * Specifies custom classname suffix to be added to the class name of created proxy.
-	 */
-	public Proxetta useClassNameSuffix(String suffix) {
-		this.classNameSuffix = suffix;
-		return this;
-	}
-
-	/**
-	 * Specifies not to append class name suffix when creating proxy class.
 	 * Warning: when class name suffix is not used, full classname has to be
 	 * specified that differs from target class name.
 	 */
-	public Proxetta dontUseClassNameSuffix() {
-		this.classNameSuffix = null;
-		return this;
+	public void setClassNameSuffix(String suffix) {
+		this.classNameSuffix = suffix;
+	}
+
+	public String getClassNameSuffix() {
+		return classNameSuffix;
 	}
 
 	// ---------------------------------------------------------------- ProxyCreator
@@ -126,180 +96,6 @@ public abstract class Proxetta {
 	/**
 	 * Creates {@link ClassProcessor} with current options.
 	 */
-	protected abstract ClassProcessor createClassProcessor();
-
-	/**
-	 * {@link #createClassProcessor() Creates} and prepares class processor.
-	 */
-	protected ClassProcessor prepareClassProcessor() {
-		ClassProcessor cp = createClassProcessor();
-		cp.setUseVariableClassName(variableClassName);
-		cp.setClassNameSuffix(classNameSuffix);
-		return cp;
-	}
-
-	// ----------------------------------------------------------------  create
-
-	/**
-	 * Generates proxy bytecode for provided class. Returns <code>null</code> if
-	 * there was no matching pointcuts and forced mode is off.
-	 */
-	public byte[] createProxy(Class target) {
-		return createProxy(target, null);
-	}
-
-	public byte[] createProxy(Class target, String proxyClassName) {
-		return createProxy(prepareClassProcessor().accept(target, proxyClassName));
-	}
-
-	/**
-	 * Generates proxy bytecode for provided class. Returns <code>null</code> if
-	 * there was no matching pointcuts and forced mode is off.
-	 */
-	public byte[] createProxy(String targetName) {
-		return createProxy(targetName, null);
-	}
-
-	public byte[] createProxy(String targetName, String proxyClassName) {
-		return createProxy(prepareClassProcessor().accept(targetName, proxyClassName));
-	}
-
-
-	/**
-	 * Generates proxy bytecode for class provided as <code>InputStream</code>. Returns <code>null</code> if
-	 * there was no matching pointcuts and forced mode is off.
-	 */
-	public byte[] createProxy(InputStream in) {
-		return createProxy(in, null);
-	}
-
-	public byte[] createProxy(InputStream in, String proxyClassName) {
-		return createProxy(prepareClassProcessor().accept(in, proxyClassName));
-	}
-
-	/**
-	 * Returns byte array of invoked proxetta creator.
-	 */
-	protected byte[] createProxy(ClassProcessor cp) {
-		byte[] result = cp.toByteArray();
-
-		if ((forced == false) && (cp.isProxyApplied() == false)) {
-			if (log.isDebugEnabled()) {
-				log.debug("proxy not applied");
-			}
-			return null;
-		}
-
-		if (log.isDebugEnabled()) {
-			log.debug("proxy created");
-		}
-
-		return result;
-	}
-
-
-	// ---------------------------------------------------------------- define
-
-	/**
-	 * Defines new proxy class.
-	 */
-	public Class defineProxy(Class target) {
-		return defineProxy(target, null);
-	}
-
-	public Class defineProxy(Class target, String proxyClassName) {
-		ClassProcessor cp = prepareClassProcessor();
-		cp.accept(target, proxyClassName);
-
-		if ((forced == false) && (cp.isProxyApplied() == false)) {
-			if (log.isDebugEnabled()) {
-				log.debug("proxy not applied on " + target.getName());
-			}
-
-			return target;
-		}
-
-		if (log.isDebugEnabled()) {
-			log.debug("proxy created on " + target.getName());
-		}
-
-		try {
-			if (classLoader == null) {
-				ClassLoader cl = target.getClassLoader();
-				if (cl == null) {
-					cl = ClassLoaderUtil.getDefaultClassLoader();
-				}
-				return ClassLoaderUtil.defineClass(cp.getProxyClassName(), cp.toByteArray(), cl);
-			}
-			return ClassLoaderUtil.defineClass(cp.getProxyClassName(), cp.toByteArray(), classLoader);
-		} catch (Exception ex) {
-			throw new ProxettaException("Proxy class definition was unsuccessful.", ex);
-		}
-	}
-
-	/**
-	 * Defines new proxy class.
-	 */
-	public Class defineProxy(String targetName) {
-	    return defineProxy(targetName, null);
-	}
-
-	public Class defineProxy(String targetName, String proxyClassName) {
-		ClassProcessor cp = prepareClassProcessor();
-		cp.accept(targetName, proxyClassName);
-
-		if ((forced == false) && (cp.isProxyApplied() == false)) {
-			if (log.isDebugEnabled()) {
-				log.debug("proxy not applied on " + targetName);
-			}
-
-			try {
-				return ClassLoaderUtil.loadClass(targetName);
-			} catch (ClassNotFoundException cnfex) {
-				throw new ProxettaException(cnfex);
-			}
-		}
-
-		if (log.isDebugEnabled()) {
-			log.debug("proxy created on " + targetName);
-		}
-
-		try {
-			if (classLoader == null) {
-				return ClassLoaderUtil.defineClass(cp.getProxyClassName(), cp.toByteArray());
-			}
-			return ClassLoaderUtil.defineClass(cp.getProxyClassName(), cp.toByteArray(), classLoader);
-		} catch (Exception ex) {
-			throw new ProxettaException("Proxy class definition was unsuccessful.", ex);
-		}
-	}
-
-	// ---------------------------------------------------------------- instance
-
-	public <T> T createProxyInstance(Class<T> target) {
-		return createProxyInstance(target, null);
-	}
-
-	@SuppressWarnings({"unchecked"})
-	public <T> T createProxyInstance(Class<T> target, String proxyClassName) {
-		Class<T> c = defineProxy(target, proxyClassName);
-		try {
-			return c.newInstance();
-		} catch (Exception ex) {
-			throw new ProxettaException("Unable to create new proxy instance.", ex);
-		}
-	}
-
-	public Object createProxyInstance(String targetName) {
-		return createProxyInstance(targetName, null);
-	}
-	public Object createProxyInstance(String targetName, String proxyClassName) {
-		Class c = defineProxy(targetName, proxyClassName);
-		try {
-			return c.newInstance();
-		} catch (Exception ex) {
-			throw new ProxettaException("Unable to create new proxy instance.", ex);
-		}
-	}
+	public abstract ClassProcessor builder();
 
 }
