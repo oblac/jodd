db57de2c811b598cf7024df7630b159924316977
Igor <igor@jodd.org>
2012-10-30 23:31:16 +0100

    annotation renamed

---
+++ jodd-joy/src/main/java/jodd/joy/madvoc/meta/JSONAction.java
@@ -0,0 +1,30 @@
+// Copyright (c) 2003-2012, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.joy.madvoc.meta;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * JSON action annotation. Extension is set to '<b>json</b>' and
+ * result type to {@link jodd.joy.madvoc.result.JSONResult json}.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface JSONAction {
+
+	String value() default "";
+
+	String extension() default "json";
+
+	String alias() default "";
+
+	String method() default "";
+
+	String result() default "json";
+
+}
