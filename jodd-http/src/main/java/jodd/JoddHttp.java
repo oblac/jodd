7d7c328af6666173128a88291591905ca75d8e98
Igor <igor@jodd.org>
2013-03-25 14:44:16 +0100

    Added default HTTP library values.

--- jodd-http/src/main/java/jodd/JoddHttp.java
+++ jodd-http/src/main/java/jodd/JoddHttp.java
@@ -2,8 +2,32 @@
 
 package jodd;
 
+import jodd.util.MimeTypes;
+import jodd.util.StringPool;
+
 /**
  * Jodd HTTP module.
  */
 public class JoddHttp {
+
+	/**
+	 * Default HTTP query parameters encoding (UTF-8).
+	 */
+	public static String defaultQueryEncoding = StringPool.UTF_8;
+
+	/**
+	 * Default form encoding (UTF-8).
+	 */
+	public static String defaultFormEncoding = StringPool.UTF_8;
+
+	/**
+	 * Default body media type (text/html).
+	 */
+	public static String defaultBodyMediaType = MimeTypes.MIME_TEXT_HTML;
+
+	/**
+	 * Default body encoding (UTF-8).
+	 */
+	public static String defaultBodyEncoding = StringPool.UTF_8;
+
 }
