12a9cb81a4a858036ac2d7e813b6d5aec0d44096
Igor <igor@jodd.org>
2013-04-17 16:18:43 +0200

    hint resolver is enhanced: supports collections and arrays

--- jodd-db/src/main/java/jodd/db/oom/JoinHintResolver.java
+++ jodd-db/src/main/java/jodd/db/oom/JoinHintResolver.java
@@ -2,9 +2,15 @@
 
 package jodd.db.oom;
 
+import jodd.introspector.ClassDescriptor;
+import jodd.introspector.ClassIntrospector;
+import jodd.util.ArraysUtil;
+import jodd.util.ReflectUtil;
 import jodd.util.StringUtil;
 import jodd.bean.BeanUtil;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -53,9 +59,56 @@ public class JoinHintResolver {
 				String key = hint.substring(0, ndx);
 				Object value = context.get(key);
 				if (value == null) {
-					throw new DbOomException("Context key '" + key + "' doesn't exist.");
+					throw new DbOomException("Hint key not found:" + key);
 				}
-				BeanUtil.setDeclaredPropertySilent(value, hint.substring(ndx + 1), data[i]);
+
+				String hintPropertyName = hint.substring(ndx + 1);
+				Class hintPropertyType = BeanUtil.getPropertyType(value, hintPropertyName);
+
+				if (hintPropertyType != null) {
+					ClassDescriptor cd = ClassIntrospector.lookup(hintPropertyType);
+
+					if (cd.isCollection()) {
+						// add element to collection
+						try {
+							Collection collection = (Collection) BeanUtil.getDeclaredProperty(value, hintPropertyName);
+
+							if (collection == null) {
+								collection = (Collection) ReflectUtil.newInstance(hintPropertyType);
+								BeanUtil.setDeclaredPropertySilent(value, hintPropertyName, collection);
+							}
+
+							collection.add(data[i]);
+						} catch (Exception ex) {
+							throw new DbOomException(ex);
+						}
+					} else if (cd.isArray()) {
+						// add element to array
+						try {
+							Object[] array = (Object[]) BeanUtil.getDeclaredProperty(value, hintPropertyName);
+
+							if (array == null) {
+								array = (Object[]) Array.newInstance(hintPropertyType.getComponentType(), 1);
+
+								BeanUtil.setDeclaredPropertySilent(value, hintPropertyName, array);
+
+								array[0] = data[i];
+							} else {
+								Object[] newArray = ArraysUtil.append(array, data[i]);
+
+								if (newArray != array) {
+									BeanUtil.setDeclaredPropertySilent(value, hintPropertyName, newArray);
+								}
+							}
+						} catch (Exception ex) {
+							throw new DbOomException(ex);
+						}
+					} else {
+						// set value
+						BeanUtil.setDeclaredPropertySilent(value, hintPropertyName, data[i]);
+					}
+				}
+
 			} else {
 				result[count] = data[i];
 				count++;
