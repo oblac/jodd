c661e59da5b5957ed00c57aad51b6219e22ae0c2
Igor <igor@jodd.org>
2013-05-16 21:40:16 +0200

    rebuild csselly lexer with jflex-1.4.3-fixes

--- jodd-lagarto/src/main/flex/jodd/csselly/csselly.sh
+++ jodd-lagarto/src/main/flex/jodd/csselly/csselly.sh
@@ -1,6 +1,6 @@
 PRJ_ROOT=../../../../../../
 
-java -jar $PRJ_ROOT/etc/jflex/JFlex-1.4.3.jar -d . csselly.flex
+java -jar $PRJ_ROOT/etc/jflex/JFlex-1.4.3-fixes.jar -d . csselly.flex
 
 mv CSSellyLexer.java ../../../java/jodd/csselly/
 
