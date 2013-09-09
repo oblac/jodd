5548c11b53282d7a50127b3fe88dc9e4526abbe0
Igor <igor@jodd.org>
2013-09-09 23:38:39 +0200

    added manual configurator for madvoc

---
+++ jodd-madvoc/src/main/java/jodd/madvoc/config/ManualMadvocConfigurator.java
@@ -0,0 +1,176 @@
+package jodd.madvoc.config;
+
+import jodd.madvoc.ActionConfig;
+import jodd.madvoc.component.ActionMethodParser;
+import jodd.madvoc.component.ActionsManager;
+import jodd.madvoc.component.MadvocConfig;
+import jodd.madvoc.component.ResultsManager;
+import jodd.madvoc.interceptor.ActionInterceptor;
+import jodd.madvoc.result.ActionResult;
+import jodd.petite.meta.PetiteInject;
+
+import java.lang.reflect.Method;
+
+/**
+ * Madvoc configurator for manual configuration.
+ */
+public abstract class ManualMadvocConfigurator implements MadvocConfigurator {
+
+	@PetiteInject
+	protected MadvocConfig madvocConfig;
+
+	@PetiteInject
+	protected ActionsManager actionsManager;
+
+	@PetiteInject
+	protected ActionMethodParser actionMethodParser;
+
+	@PetiteInject
+	protected ResultsManager resultsManager;
+
+	// ---------------------------------------------------------------- results
+
+	/**
+	 * Registers result class.
+	 */
+	public void result(Class<? extends ActionResult> resultClass) {
+		resultsManager.register(resultClass);
+	}
+
+	// ---------------------------------------------------------------- path alias
+
+	/**
+	 * Defines action path alias.
+	 */
+	public void actionPathAlias(String alias, String path) {
+		actionsManager.registerPathAlias(alias, path);
+	}
+
+	// ---------------------------------------------------------------- actions
+
+	/**
+	 * Starts with action registration.
+	 */
+	public ActionBuilder action() {
+		return new ActionBuilder();
+	}
+
+	public class ActionBuilder {
+		String method;
+		String path;
+		Class actionClass;
+		Method actionClassMethod;
+		String actionMethodString;
+		String extension;
+		String resultType;
+		Class<? extends ActionInterceptor>[] actionInterceptors;
+
+		ActionBuilder() {
+			extension = madvocConfig.getDefaultExtension();
+		}
+
+		/**
+		 * Defines action path, without the extension.
+		 */
+		public ActionBuilder path(String path) {
+			this.path = path;
+			return this;
+		}
+
+		/**
+		 * Defines HTTP method name and action path, without the extension.
+		 */
+		public ActionBuilder path(String method, String path) {
+			this.method = method;
+			this.path = path;
+			return this;
+		}
+
+		/**
+		 * Defines the extension.
+		 */
+		public ActionBuilder extension(String extension) {
+			this.extension = extension;
+			return this;
+		}
+
+		/**
+		 * Defines action class and method to map.
+		 */
+		public ActionBuilder mapTo(Class actionClass, Method actionMethod) {
+			this.actionClass = actionClass;
+			this.actionClassMethod = actionMethod;
+			this.actionMethodString = null;
+			return this;
+		}
+
+		/**
+		 * Defines action method. It must be declared in action class.
+		 */
+		public ActionBuilder mapTo(Method actionMethod) {
+			this.actionClass = actionMethod.getDeclaringClass();
+			this.actionClassMethod = actionMethod;
+			this.actionMethodString = null;
+			return this;
+		}
+
+		/**
+		 * Defines action class and method to map.
+		 */
+		public ActionBuilder mapTo(Class actionClass, String actionMethodName) {
+			this.actionClass = actionClass;
+			this.actionClassMethod = null;
+			this.actionMethodString = actionMethodName;
+			return this;
+		}
+
+		/**
+		 * Defines set of interceptors.
+		 */
+		public ActionBuilder interceptedBy(Class<? extends ActionInterceptor>... interceptors) {
+			this.actionInterceptors = interceptors;
+			return this;
+		}
+
+		/**
+		 * Defines result type. Note that result class must be
+		 * already {@link #result(Class) registered.}
+		 */
+		public ActionBuilder result(String resultType) {
+			this.resultType = resultType;
+			return this;
+		}
+
+		/**
+		 * Defines and, optionally, registers resut type.
+		 */
+		public ActionBuilder result(Class<? extends ActionResult> resultClass) {
+			ActionResult actionResult = resultsManager.lookup(resultClass);
+
+			if (actionResult == null) {
+				actionResult = resultsManager.register(resultClass);
+			}
+			resultType = actionResult.getType();
+			return this;
+		}
+
+		/**
+		 * Binds and finalize action configuration.
+		 */
+		public void bind() {
+			if (actionMethodString != null) {
+				actionClassMethod = actionMethodParser.resolveActionMethod(actionClass, actionMethodString);
+			}
+
+			ActionConfig actionConfig =
+					actionMethodParser.createActionConfig(
+							actionClass, actionClassMethod,
+							actionInterceptors, path, method,
+							extension, resultType);
+
+			actionsManager.registerAction(actionConfig);
+		}
+
+	}
+
+}
---
+++ jodd-madvoc/src/test/java/jodd/madvoc/ManualRegistrationTest.java
@@ -0,0 +1,44 @@
+//  Copyright (c) 2003-2013, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.madvoc;
+
+import jodd.madvoc.component.ActionsManager;
+import jodd.madvoc.config.ManualMadvocConfigurator;
+import jodd.madvoc.interceptor.EchoInterceptor;
+import jodd.madvoc.result.TextResult;
+import jodd.madvoc.tst.BooAction;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ManualRegistrationTest {
+
+	public static class ManualRegistration extends ManualMadvocConfigurator {
+		public void configure() {
+			action().path("/hello").mapTo(BooAction.class, "foo1").result(TextResult.class).bind();
+			action().path("/world").mapTo(BooAction.class, "foo2").interceptedBy(EchoInterceptor.class).result(TextResult.class).bind();
+		}
+	}
+
+	@Test
+	public void testManualAction() {
+		Madvoc madvoc = new Madvoc();
+		madvoc.setMadvocConfiguratorClass(ManualRegistration.class);
+		madvoc.startNewWebApplication(null);
+
+		ActionsManager actionsManager = madvoc.getWebApplication().getComponent(ActionsManager.class);
+
+		assertEquals(2, actionsManager.getActionsCount());
+
+		ActionConfig actionConfig = actionsManager.lookup("/hello", "GET");
+		assertNotNull(actionConfig);
+		assertEquals(BooAction.class, actionConfig.getActionClass());
+		assertEquals("foo1", actionConfig.actionClassMethod.getName());
+
+		actionConfig = actionsManager.lookup("/world", "GET");
+		assertNotNull(actionConfig);
+		assertEquals(BooAction.class, actionConfig.getActionClass());
+		assertEquals("foo2", actionConfig.actionClassMethod.getName());
+	}
+}
