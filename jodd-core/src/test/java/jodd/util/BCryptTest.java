c54f8d5343ad7a73dbcbcb761ab316ee99ab4530
Igor <igor@jodd.org>
2013-11-08 22:47:57 +0100

    Added BCrypt test

---
+++ jodd-core/src/test/java/jodd/util/BCryptTest.java
@@ -0,0 +1,24 @@
+package jodd.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BCryptTest {
+
+	@Test
+	public void testBCrypt() {
+		String hash = BCrypt.hashpw("password", BCrypt.gensalt(7));
+		assertTrue(BCrypt.checkpw("password", hash));
+	}
+
+	@Test
+	public void testBCryptRandom() {
+		for (int rounds = 0; rounds< 1000; rounds++) {
+			String text = RandomStringUtil.randomAlphaNumeric(10);
+
+			String hash = BCrypt.hashpw(text, BCrypt.gensalt(4));
+			assertTrue(BCrypt.checkpw(text, hash));
+		}
+	}
+}
