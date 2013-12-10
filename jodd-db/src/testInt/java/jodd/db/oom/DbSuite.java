fdfb308b8b4f4f92347a284bf042989b51d17e32
Igor <igor@jodd.org>
2013-12-10 14:09:51 +0100

    added db suite for db integration tests

---
+++ jodd-db/src/testInt/java/jodd/db/oom/DbSuite.java
@@ -0,0 +1,11 @@
+package jodd.db.oom;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		LiveDatabaseTest.class
+})
+public class DbSuite {
+}
