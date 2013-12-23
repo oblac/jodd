9b0d74e789e1b47820cd257140e746a9f76b309d
Igor <igor@jodd.org>
2013-12-23 12:34:01 +0100

    Removed BeanLoaders - no real value.

--- jodd-bean/src/main/java/jodd/bean/loader/package-info.java
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2003-2013, Jodd Team (jodd.org). All Rights Reserved.
-
-/**
- * Bean loaders iterate given bean, read property names and values which are stored into destination bean.
- */
-package jodd.bean.loader;
