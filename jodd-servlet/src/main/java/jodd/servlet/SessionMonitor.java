80226069c91952eef5a5f0d4acba9e9c5e9fdbf4
Igor <igor@jodd.org>
2014-01-09 23:27:10 +0100

    Added SessionMonitor instead of SessionMapListener and previously removed HttpSessionListenerBroadcaster.

---
+++ jodd-servlet/src/main/java/jodd/servlet/SessionMonitor.java
@@ -0,0 +1,96 @@
+// Copyright (c) 2003-2014, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.servlet;
+
+import jodd.util.ref.ReferenceMap;
+import jodd.util.ref.ReferenceType;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.List;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Session monitor keep map of sessions and allow registration of
+ * session listeners in runtime.
+ */
+public class SessionMonitor implements HttpSessionListener {
+
+	protected static SessionMonitor sessionMonitor;
+
+	protected List<HttpSessionListener> listeners;
+	protected ConcurrentMap<String, HttpSession> sessionMap;
+
+	/**
+	 * Creates new session monitor.
+	 */
+	public SessionMonitor() {
+		if (sessionMonitor != null) {
+			sessionMonitor = this;
+			listeners = new CopyOnWriteArrayList<HttpSessionListener>();
+			sessionMap = new ReferenceMap<String, HttpSession>(ReferenceType.STRONG, ReferenceType.WEAK);
+		}
+	}
+
+	/**
+	 * Returns singleton instance of <code>SessionMonitor</code>.
+	 */
+	public static SessionMonitor getInstance() {
+		return sessionMonitor;
+	}
+
+	// ---------------------------------------------------------------- listeners
+
+	/**
+	 * Registers new session listener.
+	 */
+	public void registerListener(HttpSessionListener listener) {
+		listeners.add(listener);
+	}
+
+	/**
+	 * Removes existing session listener.
+	 */
+	public void removeListener(HttpSessionListener listener) {
+		listeners.remove(listener);
+	}
+
+	// ---------------------------------------------------------------- broadcast
+
+	/**
+	 * Stores session in map and broadcasts event to registered listeners.
+	 */
+	public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+		HttpSession session = httpSessionEvent.getSession();
+		sessionMap.putIfAbsent(session.getId(), session);
+
+		for (HttpSessionListener listener : listeners) {
+			listener.sessionCreated(httpSessionEvent);
+		}
+	}
+
+	/**
+	 * Removes session from a map and broadcasts event to registered listeners.
+	 */
+	public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+		HttpSession session = httpSessionEvent.getSession();
+		sessionMap.remove(session.getId());
+
+		for (HttpSessionListener listener : listeners) {
+			listener.sessionDestroyed(httpSessionEvent);
+		}
+	}
+
+	// ---------------------------------------------------------------- map
+
+	/**
+	 * Returns session for given session id. Returns <code>null</code>
+	 * if session expired.
+	 */
+	public HttpSession getSession(String sessionId) {
+		return sessionMap.get(sessionId);
+	}
+
+}
