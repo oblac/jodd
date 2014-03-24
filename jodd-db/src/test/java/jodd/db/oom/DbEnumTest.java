9aef4640fe04a809b7eac8a7d691aa2658ad6326
Igor <igor@jodd.org>
2014-03-24 19:56:37 +0100

    Added db enumerator test

---
+++ jodd-db/src/test/java/jodd/db/oom/DbEnumTest.java
@@ -0,0 +1,47 @@
+// Copyright (c) 2003-2014, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.db.oom;
+
+import jodd.db.DbHsqldbTestCase;
+import jodd.db.DbQuery;
+import jodd.db.DbSession;
+import jodd.db.DbThreadSession;
+import jodd.db.oom.tst.Enumerator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static jodd.db.oom.sqlgen.DbEntitySql.insert;
+
+public class DbEnumTest extends DbHsqldbTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+
+		DbOomManager.resetAll();
+		DbOomManager dbOom = DbOomManager.getInstance();
+		dbOom.registerEntity(Enumerator.class);
+	}
+
+	@Test
+	public void testEnums() {
+		DbSession session = new DbThreadSession(cp);
+
+		String sql = "create table ENUMERATOR(ID int, NAME varchar(20), STATUS int)";
+
+		DbQuery query = new DbQuery(sql);
+		query.executeUpdateAndClose();
+
+		Enumerator e = new Enumerator();
+		e.id = 2;
+		e.name = "Ikigami";
+		e.status = Enumerator.STATUS.ONE;
+
+		DbSqlGenerator gen = insert(e);
+		query = new DbOomQuery(gen);
+		query.executeUpdateAndClose();
+
+		session.closeSession();
+	}
+
+}
---
+++ jodd-db/src/test/java/jodd/db/oom/tst/Enumerator.java
@@ -0,0 +1,39 @@
+// Copyright (c) 2003-2014, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.db.oom.tst;
+
+import jodd.db.oom.meta.DbColumn;
+import jodd.db.oom.meta.DbId;
+import jodd.db.oom.meta.DbTable;
+
+@DbTable
+public class Enumerator {
+
+	public static enum STATUS {
+		ONE(1),
+		TWO(123),
+		THREE(222);
+
+		final int value;
+
+		private STATUS(int i) {
+			this.value = i;
+		}
+
+		public int value() {
+			return value;
+		}
+
+		@Override
+		public String toString() {
+			return Integer.toString(value);
+		}
+	}
+
+
+	@DbId public long id;
+	@DbColumn public String name;
+	@DbColumn public STATUS status;
+
+
+}
