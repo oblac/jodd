5a212f3a21e54f33d0363a9f4910286c01d0cea8
Igor Spasić <igor@jodd.org>
2014-07-02 23:57:30 +0200

    Rebuild the CSSelly Lexer with JFlex 1.6.0

--- jodd-lagarto/src/main/flex/jodd/csselly/csselly.sh
+++ jodd-lagarto/src/main/flex/jodd/csselly/csselly.sh
@@ -4,7 +4,7 @@ set -e
 PRJ_ROOT=../../../../../../
 JFLEX_ROOT=$PRJ_ROOT/etc/jflex
 
-java -jar $JFLEX_ROOT/JFlex-1.4.3-fixes.jar --skel $JFLEX_ROOT/skeleton.chararray.jodd -d . csselly.flex
+java -jar $JFLEX_ROOT/JFlex-1.6.0.jar --skel $JFLEX_ROOT/skeleton.chararray-1.6.0.jodd -d . csselly.flex
 
 mv CSSellyLexer.java ../../../java/jodd/csselly/
 
