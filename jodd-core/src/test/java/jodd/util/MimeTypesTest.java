fa792d9d3bc56bf38a4352f88444a763910c3d46
Igor Spasić <igor@jodd.org>
2014-08-11 11:57:54 +0200

    Fixed a test

--- jodd-core/src/test/java/jodd/util/MimeTypesTest.java
+++ jodd-core/src/test/java/jodd/util/MimeTypesTest.java
@@ -27,7 +27,7 @@ public class MimeTypesTest {
 	public void testFind() {
 		String[] extensionArray = MimeTypes.findExtensionsByMimeTypes("image/jpeg", false);
 
-		String extensions = StringUtil.toString(extensionArray) + ',';
+		String extensions = ArraysUtil.toString(extensionArray) + ',';
 
 		assertEquals(3, extensionArray.length);
 
@@ -45,7 +45,7 @@ public class MimeTypesTest {
 	public void testFindWithWildcards() {
 		String[] extensionArray = MimeTypes.findExtensionsByMimeTypes("image/*", true);
 
-		String extensions = StringUtil.toString(extensionArray) + ',';
+		String extensions = ArraysUtil.toString(extensionArray) + ',';
 
 		assertTrue(extensions.length() > 3);
 
