b942b3fb05d4d3a23c572bff09dc88bb65aa525f
Igor Spasić <igor@jodd.org>
2014-08-20 22:09:18 +0200

    Added dynamic macros for COPY operator

--- jodd-props/src/main/java/jodd/props/PropsParser.java
+++ jodd-props/src/main/java/jodd/props/PropsParser.java
@@ -412,19 +412,45 @@ public class PropsParser implements Cloneable {
 			String[] sources = StringUtil.splitc(value, ',');
 			for (String source : sources) {
 				source = source.trim();
+
+				// try to extract profile for parsing
+
+				String[] lookupProfiles = profiles;
+				String lookupProfilesString = null;
+
+				int leftIndex = source.indexOf('<');
+				if (leftIndex != -1) {
+					int rightIndex = source.indexOf('>');
+
+					lookupProfilesString = source.substring(leftIndex + 1, rightIndex);
+					source = source.substring(0, leftIndex).concat(source.substring(rightIndex + 1));
+
+					lookupProfiles = StringUtil.splitc(lookupProfilesString, ',');
+
+					StringUtil.trimAll(lookupProfiles);
+				}
+
 				String[] wildcards = new String[] {source + ".*"};
-				propsData.extract(target, profiles, wildcards);
+
+				propsData.extract(target, lookupProfiles, wildcards);
 
 				for (Map.Entry<String, Object> entry : target.entrySet()) {
 					String entryKey = entry.getKey();
 					String suffix = entryKey.substring(source.length());
 
 					String newKey = key + suffix;
+
+					String newValue = "${" + entryKey;
+					if (lookupProfilesString != null) {
+						newValue += "<" + lookupProfilesString + ">";
+					}
+					newValue += "}";
+
 					if (profiles == null) {
-						propsData.putBaseProperty(newKey, "${" + entryKey + "}", false);
+						propsData.putBaseProperty(newKey, newValue, false);
 					} else {
-						for (final String p : keyProfiles) {
-							propsData.putProfileProperty(newKey, "${" + entryKey + "}", p, false);
+						for (final String p : profiles) {
+							propsData.putProfileProperty(newKey, newValue, p, false);
 						}
 					}
 				}
