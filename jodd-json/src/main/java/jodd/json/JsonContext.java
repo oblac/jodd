34043efeedc4fa95cc8329c28782ccd4e30a36b8
Igor Spasić <igor@jodd.org>
2014-08-24 22:29:00 +0200

    JsonContext split to JsonWriter.

--- jodd-json/src/main/java/jodd/json/JsonContext.java
+++ jodd-json/src/main/java/jodd/json/JsonContext.java
@@ -6,10 +6,8 @@ import jodd.JoddJson;
 import jodd.introspector.ClassDescriptor;
 import jodd.introspector.ClassIntrospector;
 import jodd.util.ReflectUtil;
-import jodd.util.StringPool;
 import jodd.util.Wildcard;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,18 +16,17 @@ import static jodd.util.StringPool.NULL;
 /**
  * JSON context used during serialization for building the JSON string.
  */
-public class JsonContext {
+public class JsonContext extends JsonWriter {
 
 	// ---------------------------------------------------------------- ctor
 
 	protected final JsonSerializer jsonSerializer;
-	protected final Appendable out;
 	protected final List<Object> bag;
 	protected final Path path;
 
 	public JsonContext(JsonSerializer jsonSerializer, Appendable appendable) {
+		super(appendable);
 		this.jsonSerializer = jsonSerializer;
-		this.out = appendable;
 		this.bag = new ArrayList<Object>();
 		this.path = new Path();
 	}
@@ -47,7 +44,6 @@ public class JsonContext {
 	 */
 	public boolean isUsed(Object value) {
 		for (int i = 0; i < bag.size(); i++) {
-
 			if (bag.get(i) == value) {
 				return true;
 			}
@@ -109,188 +105,6 @@ public class JsonContext {
 		}
 
 		typeJsonSerializer.serialize(this, object);
-	}
-
-	// ---------------------------------------------------------------- push
-
-	protected String pushedName;
-	protected boolean pushedComma;
-
-	/**
-	 * Stores name to temporary stack. Used when name's value may or may not be
-	 * serialized (e.g. it may be excluded), in that case we do not need to
-	 * write the name.
-	 */
-	public void pushName(String name, boolean withComma) {
-		pushedName = name;
-		pushedComma = withComma;
-	}
-
-	/**
-	 * Writes stored name to JSON string. Cleans storage.
-	 */
-	protected void popName() {
-		if (pushedName != null) {
-			if (pushedComma) {
-				writeComma();
-			}
-			String name = pushedName;
-			pushedName = null;
-			writeName(name);
-		}
-	}
-
-	/**
-	 * Returns <code>true</code> if {@link #pushName(String, boolean)}  pushed name}
-	 * has been {@link #popName() poped, i.e. used}.
-	 */
-	public boolean isNamePoped() {
-		boolean b = pushedName == null;
-		pushedName = null;
-		return b;
-	}
-
-	// ---------------------------------------------------------------- write
-
-	/**
-	 * Writes open object sign.
-	 */
-	public void writeOpenObject() {
-		popName();
-		write('{');
-	}
-
-	/**
-	 * Writes close object sign.
-	 */
-	public void writeCloseObject() {
-		write('}');
-	}
-
-	/**
-	 * Writes object's property name: string and a colon.
-	 */
-	public void writeName(String name) {
-		if (name != null) {
-			writeString(name);
-		}
-		else {
-			write(NULL);
-		}
-
-		write(':');
-	}
-
-	/**
-	 * Writes open array sign.
-	 */
-	public void writeOpenArray() {
-		popName();
-		write('[');
-	}
-
-	/**
-	 * Writes close array sign.
-	 */
-	public void writeCloseArray() {
-		write(']');
-	}
-
-	/**
-	 * Write a quoted and escaped value to the output.
-	 */
-	public void writeString(String value) {
-		popName();
-
-		write(StringPool.QUOTE);
-
-		int len = value.length();
-
-		for (int i = 0; i < len; i++) {
-			char c = value.charAt(i);
-
-			switch (c) {
-				case '"':
-					write("\\\"");
-					break;
-				case '\\':
-					write("\\\\");
-					break;
-				case '/':
-					write("\\/");
-					break;
-				case '\b':
-					write("\\b");
-					break;
-				case '\f':
-					write("\\f");
-					break;
-				case '\n':
-					write("\\n");
-					break;
-				case '\r':
-					write("\\r");
-					break;
-				case '\t':
-					write("\\t");
-					break;
-				default:
-					if (Character.isISOControl(c)) {
-						unicode(c);
-					}
-					else {
-						write(c);
-					}
-			}
-		}
-
-		write(StringPool.QUOTE);
-	}
-
-	/**
-	 * Writes unicode representation of a character.
-	 */
-	protected void unicode(char c) {
-		write("\\u");
-		int n = c;
-		for (int i = 0; i < 4; ++i) {
-			int digit = (n & 0xf000) >> 12;
-			write(HEX[digit]);
-			n <<= 4;
-		}
-	}
-
-	/**
-	 * Writes comma.
-	 */
-	public void writeComma() {
-		write(',');
-	}
-
-
-	private static final char[] HEX = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
-	/**
-	 * Appends char sequence to the buffer. Used for numbers, nulls, booleans, etc.
-	 */
-	public void write(CharSequence charSequence) {
-		popName();
-		try {
-			out.append(charSequence);
-		} catch (IOException ioex) {
-			throw new JsonException(ioex);
-		}
-	}
-
-	/**
-	 * Appends char to the buffer. Used internally.
-	 */
-	protected void write(char c) {
-		try {
-			out.append(c);
-		} catch (IOException ioex) {
-			throw new JsonException(ioex);
-		}
 	}
 
 	// ---------------------------------------------------------------- matchers
---
+++ jodd-json/src/main/java/jodd/json/JsonWriter.java
@@ -0,0 +1,205 @@
+// Copyright (c) 2003-2014, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.json;
+
+import jodd.util.CharUtil;
+import jodd.util.StringPool;
+
+import java.io.IOException;
+
+import static jodd.util.StringPool.NULL;
+
+/**
+ * Simple JSON writer.
+ */
+public class JsonWriter {
+
+	protected final Appendable out;
+
+	public JsonWriter(Appendable out) {
+		this.out = out;
+	}
+
+	// ---------------------------------------------------------------- push
+
+	protected String pushedName;
+	protected boolean pushedComma;
+
+	/**
+	 * Stores name to temporary stack. Used when name's value may or may not be
+	 * serialized (e.g. it may be excluded), in that case we do not need to
+	 * write the name.
+	 */
+	public void pushName(String name, boolean withComma) {
+		pushedName = name;
+		pushedComma = withComma;
+	}
+
+	/**
+	 * Writes stored name to JSON string. Cleans storage.
+	 */
+	protected void popName() {
+		if (pushedName != null) {
+			if (pushedComma) {
+				writeComma();
+			}
+			String name = pushedName;
+			pushedName = null;
+			writeName(name);
+		}
+	}
+
+	/**
+	 * Returns <code>true</code> if {@link #pushName(String, boolean)}  pushed name}
+	 * has been {@link #popName() poped, i.e. used}.
+	 */
+	public boolean isNamePoped() {
+		boolean b = pushedName == null;
+		pushedName = null;
+		return b;
+	}
+
+	// ---------------------------------------------------------------- write
+
+	/**
+	 * Writes open object sign.
+	 */
+	public void writeOpenObject() {
+		popName();
+		write('{');
+	}
+
+	/**
+	 * Writes close object sign.
+	 */
+	public void writeCloseObject() {
+		write('}');
+	}
+
+	/**
+	 * Writes object's property name: string and a colon.
+	 */
+	public void writeName(String name) {
+		if (name != null) {
+			writeString(name);
+		}
+		else {
+			write(NULL);
+		}
+
+		write(':');
+	}
+
+	/**
+	 * Writes open array sign.
+	 */
+	public void writeOpenArray() {
+		popName();
+		write('[');
+	}
+
+	/**
+	 * Writes close array sign.
+	 */
+	public void writeCloseArray() {
+		write(']');
+	}
+
+	/**
+	 * Write a quoted and escaped value to the output.
+	 */
+	public void writeString(String value) {
+		popName();
+
+		write(StringPool.QUOTE);
+
+		int len = value.length();
+
+		for (int i = 0; i < len; i++) {
+			char c = value.charAt(i);
+
+			switch (c) {
+				case '"':
+					write("\\\"");
+					break;
+				case '\\':
+					write("\\\\");
+					break;
+				case '/':
+					write("\\/");
+					break;
+				case '\b':
+					write("\\b");
+					break;
+				case '\f':
+					write("\\f");
+					break;
+				case '\n':
+					write("\\n");
+					break;
+				case '\r':
+					write("\\r");
+					break;
+				case '\t':
+					write("\\t");
+					break;
+				default:
+					if (Character.isISOControl(c)) {
+						unicode(c);
+					}
+					else {
+						write(c);
+					}
+			}
+		}
+
+		write(StringPool.QUOTE);
+	}
+
+	/**
+	 * Writes unicode representation of a character.
+	 */
+	protected void unicode(char c) {
+		write("\\u");
+		int n = c;
+		for (int i = 0; i < 4; ++i) {
+			int digit = (n & 0xf000) >> 12;
+			char hex = CharUtil.int2hex(digit);
+
+			write(hex);
+
+			n <<= 4;
+		}
+	}
+
+	/**
+	 * Writes comma.
+	 */
+	public void writeComma() {
+		write(',');
+	}
+
+	/**
+	 * Appends char sequence to the buffer. Used for numbers, nulls, booleans, etc.
+	 */
+	public void write(CharSequence charSequence) {
+		popName();
+		try {
+			out.append(charSequence);
+		} catch (IOException ioex) {
+			throw new JsonException(ioex);
+		}
+	}
+
+	/**
+	 * Appends char to the buffer. Used internally.
+	 */
+	protected void write(char c) {
+		try {
+			out.append(c);
+		} catch (IOException ioex) {
+			throw new JsonException(ioex);
+		}
+	}
+
+}
