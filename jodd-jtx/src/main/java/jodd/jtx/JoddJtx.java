a110ace1c7def8c7370c894774e11fb48ef3b4b5
Igor Spasić <igor@jodd.org>
2014-08-25 16:39:42 +0200

    modules moved to packages

--- jodd-jtx/src/main/java/jodd/jtx/JoddJtx.java
+++ jodd-jtx/src/main/java/jodd/jtx/JoddJtx.java
@@ -1,6 +1,8 @@
 // Copyright (c) 2003-2014, Jodd Team (jodd.org). All Rights Reserved.
 
-package jodd;
+package jodd.jtx;
+
+import jodd.Jodd;
 
 /**
  * Jodd JTX module.
@@ -8,7 +10,11 @@ package jodd;
 public class JoddJtx {
 
 	static {
-		Jodd.module();
+		init();
+	}
+
+	public static void init() {
+		Jodd.init(JoddJtx.class);
 	}
 
 }
--- jodd-lagarto/src/main/java/jodd/lagarto/JoddLagarto.java
+++ jodd-lagarto/src/main/java/jodd/lagarto/JoddLagarto.java
@@ -1,6 +1,8 @@
 // Copyright (c) 2003-2014, Jodd Team (jodd.org). All Rights Reserved.
 
-package jodd;
+package jodd.lagarto;
+
+import jodd.Jodd;
 
 /**
  * Jodd LAGARTO module.
@@ -8,7 +10,11 @@ package jodd;
 public class JoddLagarto {
 
 	static {
-		Jodd.module();
+		init();
+	}
+
+	public static void init() {
+		Jodd.init(JoddLagarto.class);
 	}
 
 }
