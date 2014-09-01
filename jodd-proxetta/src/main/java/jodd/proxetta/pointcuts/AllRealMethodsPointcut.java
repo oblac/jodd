3843c644efd505ea70a07f1a8c2f48265eb5812e
Igor Spasić <igor@jodd.org>
2014-09-01 23:52:33 +0200

    Added new pointcut for all methods but not getters/setters

---
+++ jodd-proxetta/src/main/java/jodd/proxetta/pointcuts/AllRealMethodsPointcut.java
@@ -0,0 +1,29 @@
+// Copyright (c) 2003-2014, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.proxetta.pointcuts;
+
+import jodd.proxetta.MethodInfo;
+
+/**
+ * Pointcut on all public methods that are <b>not</b> getters or setters
+ */
+public class AllRealMethodsPointcut extends ProxyPointcutSupport {
+
+	public boolean apply(MethodInfo methodInfo) {
+
+		if (hasReturnValue(methodInfo)
+				&& (matchMethodName(methodInfo, "get*") || (matchMethodName(methodInfo, "is*")))
+				&& hasNoArguments(methodInfo)) {
+			// getter
+			return false;
+		}
+
+		if (matchMethodName(methodInfo, "set*")
+				&& hasOneArgument(methodInfo)) {
+			// setter
+			return false;
+		}
+
+		return isPublic(methodInfo);
+	}
+}
