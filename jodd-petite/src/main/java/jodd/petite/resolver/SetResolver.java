707eccf7f1a7c478c9651918f528c309ef32a191
Igor Spasić <igor@jodd.org>
2014-09-02 09:44:36 +0200

    And this can be written better

--- jodd-petite/src/main/java/jodd/petite/resolver/SetResolver.java
+++ jodd-petite/src/main/java/jodd/petite/resolver/SetResolver.java
@@ -12,8 +12,6 @@ import jodd.petite.SetInjectionPoint;
 import jodd.petite.meta.PetiteInject;
 import jodd.util.ReflectUtil;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -55,17 +53,10 @@ public class SetResolver {
 			PetiteInject ref = null;
 
 			if (writeMethodDescriptor != null) {
-				Method method = writeMethodDescriptor.getMethod();
-
-				ref = method.getAnnotation(PetiteInject.class);
+				ref = writeMethodDescriptor.getMethod().getAnnotation(PetiteInject.class);
 			}
-
-			if (ref == null) {
-				if (fieldDescriptor != null) {
-					Field field = fieldDescriptor.getField();
-
-					ref = field.getAnnotation(PetiteInject.class);
-				}
+			if (ref == null && fieldDescriptor != null) {
+				ref = fieldDescriptor.getField().getAnnotation(PetiteInject.class);
 			}
 
 			if ((autowire == false) && (ref == null)) {
