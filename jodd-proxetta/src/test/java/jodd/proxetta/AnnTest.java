2b368292feec6c820d76b8c2464484d10b40b076
Igor Spasić <igor@jodd.org>
2014-09-22 18:43:15 +0200

    Test added

--- jodd-proxetta/src/test/java/jodd/proxetta/AnnTest.java
+++ jodd-proxetta/src/test/java/jodd/proxetta/AnnTest.java
@@ -4,15 +4,20 @@ import jodd.proxetta.data.Hero;
 import jodd.proxetta.data.HeroProxyAdvice;
 import jodd.proxetta.impl.ProxyProxetta;
 
+import jodd.proxetta.impl.WrapperProxetta;
+import jodd.proxetta.impl.WrapperProxettaBuilder;
 import jodd.proxetta.pointcuts.AllRealMethodsPointcut;
+import jodd.util.ReflectUtil;
 import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.Assert.assertEquals;
 
 public class AnnTest {
 
 	@Test
-	public void testMethodAnnotations() {
+	public void testMethodAnnotations1() {
 		ProxyProxetta proxyProxetta = ProxyProxetta
 				.withAspects(
 					new ProxyAspect(HeroProxyAdvice.class,
@@ -33,6 +38,30 @@ public class AnnTest {
 		Hero hero = (Hero) proxettaBuilder.newInstance();
 
 		assertEquals("BatmanHero37W88.3CatWoman99speeeeedXRAY", hero.name());
+	}
+
+	@Test
+	public void testMethodAnnotations2() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+		WrapperProxetta proxyProxetta = WrapperProxetta
+				.withAspects(
+					new ProxyAspect(HeroProxyAdvice.class,
+							new AllRealMethodsPointcut() {
+								@Override
+								public boolean apply(MethodInfo methodInfo) {
+									if (!methodInfo.isTopLevelMethod()) {
+										return false;
+									}
+									return super.apply(methodInfo);
+								}
+							}))
+				//.setDebugFolder("/Users/igor/")
+				;
+
+		WrapperProxettaBuilder proxettaBuilder = proxyProxetta.builder();
+		proxettaBuilder.setTarget(Hero.class);
+		Object hero = proxettaBuilder.newInstance();
+
+		assertEquals("BatmanHero37W88.3CatWoman99speeeeedXRAY", ReflectUtil.invoke(hero, "name"));
 	}
 
 }
