c502988adb720a395a626d39e3768ab9a7fe0d78
Igor Spasić <igor@jodd.org>
2014-09-27 00:18:45 +0200

    Added simple hashcode test

---
+++ jodd-core/src/test/java/jodd/util/HashCodeTest.java
@@ -0,0 +1,31 @@
+// Copyright (c) 2003-2014, Jodd Team (jodd.org). All Rights Reserved.
+
+package jodd.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HashCodeTest {
+
+	@Test
+	public void testhashCode() {
+		int hash = HashCode.hash(0, "Hey");
+		hash = HashCode.hash(hash, 1);
+		hash = HashCode.hash(hash, 1.4);
+		hash = HashCode.hash(hash, 9f);
+		hash = HashCode.hash(hash, true);
+		hash = HashCode.hash(hash, ArraysUtil.ints(1,2,3,4));
+
+		int hash2 = hash;
+
+		hash = HashCode.hash(0, "Hey");
+		hash = HashCode.hash(hash, 1);
+		hash = HashCode.hash(hash, 1.4);
+		hash = HashCode.hash(hash, 9f);
+		hash = HashCode.hash(hash, true);
+		hash = HashCode.hash(hash, ArraysUtil.ints(1,2,3,4));
+
+		assertEquals(hash, hash2);
+	}
+}
