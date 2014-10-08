1d1bbe7a46580f8479e2a6055b74cd369048b46f
Igor Spasić <igor@jodd.org>
2014-10-08 14:04:13 +0200

    Add more stuff to Uploadable

--- jodd-http/src/main/java/jodd/http/up/Uploadable.java
+++ jodd-http/src/main/java/jodd/http/up/Uploadable.java
@@ -2,6 +2,9 @@
 
 package jodd.http.up;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Common interface of uploaded content for {@link jodd.http.HttpBase#form() form parameters}.
  * All supported objects that can be uploaded using
@@ -32,5 +35,16 @@ public interface Uploadable<T> {
 	 * {@link #getFileName() file name's} extension.
 	 */
 	public String getMimeType();
+
+	/**
+	 * Returns size in bytes.
+	 */
+	public int getSize();
+
+	/**
+	 * Opens <code>InputStream</code>. User is responsible
+	 * for closing it.
+	 */
+	public InputStream openInputStream() throws IOException;
 
 }
