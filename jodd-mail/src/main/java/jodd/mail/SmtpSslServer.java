5eb41bc712f35bf76588892e91abc4dafe391849
Igor Spasić <igor@jodd.org>
2015-03-28 23:11:44 +0100

    Added startTlsRequired() and plaintextOverTLS() methods to SmtpSslServer (closes #210)

--- jodd-mail/src/main/java/jodd/mail/SmtpSslServer.java
+++ jodd-mail/src/main/java/jodd/mail/SmtpSslServer.java
@@ -11,6 +11,7 @@ import java.util.Properties;
  */
 public class SmtpSslServer extends SmtpServer<SmtpSslServer> {
 
+	public static final String MAIL_SMTP_STARTTLS_REQUIRED 		= "mail.smtp.starttls.required";
 	public static final String MAIL_SMTP_STARTTLS_ENABLE 		= "mail.smtp.starttls.enable";
 	public static final String MAIL_SMTP_SOCKET_FACTORY_PORT 	= "mail.smtp.socketFactory.port";
 	public static final String MAIL_SMTP_SOCKET_FACTORY_CLASS 	= "mail.smtp.socketFactory.class";
@@ -34,15 +35,49 @@ public class SmtpSslServer extends SmtpServer<SmtpSslServer> {
 		super(host, port);
 	}
 
+	// ---------------------------------------------------------------- properties
+
+	protected boolean startTlsRequired = false;
+	protected boolean plaintextOverTLS = false;
+
+	/**
+	 * Sets <code>mail.smtp.starttls.required</code> which according to
+	 * Java Mail API means: If true, requires the use of the STARTTLS command.
+	 * If the server doesn't support the STARTTLS command, or the command fails,
+	 * the connect method will fail. Defaults to <code>false</code>.
+	 */
+	public SmtpSslServer startTlsRequired(boolean startTlsRequired) {
+		this.startTlsRequired = startTlsRequired;
+		return this;
+	}
+
+	/**
+	 * When enabled, SMTP socket factory class will be not set,
+	 * and Plaintext Authentication over TLS will be enabled.
+	 */
+	public SmtpSslServer plaintextOverTLS(boolean plaintextOverTLS) {
+		this.plaintextOverTLS = plaintextOverTLS;
+		return this;
+	}
+
 
 	@Override
 	protected Properties createSessionProperties() {
 		Properties props = super.createSessionProperties();
 
+		props.setProperty(MAIL_SMTP_STARTTLS_REQUIRED,
+			startTlsRequired ? StringPool.TRUE : StringPool.FALSE);
+
 		props.setProperty(MAIL_SMTP_STARTTLS_ENABLE, StringPool.TRUE);
+
 		props.setProperty(MAIL_SMTP_SOCKET_FACTORY_PORT, String.valueOf(port));
+
 		props.setProperty(MAIL_SMTP_PORT, String.valueOf(port));
-		props.setProperty(MAIL_SMTP_SOCKET_FACTORY_CLASS, "javax.net.ssl.SSLSocketFactory");
+
+		if (!plaintextOverTLS) {
+			props.setProperty(MAIL_SMTP_SOCKET_FACTORY_CLASS, "javax.net.ssl.SSLSocketFactory");
+		}
+
 		props.setProperty(MAIL_SMTP_SOCKET_FACTORY_FALLBACK, StringPool.FALSE);
 		props.setProperty(MAIL_HOST, host);
 
