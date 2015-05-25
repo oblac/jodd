4fbcf0b7f193f4a4909840a1cb41698c885ba7a7
Igor Spasić <igor@jodd.org>
2015-05-25 17:01:21 +0200

    removed unused import

--- jodd-http/src/main/java/jodd/http/HttpResponse.java
+++ jodd-http/src/main/java/jodd/http/HttpResponse.java
@@ -11,7 +11,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.zip.GZIPInputStream;
 
