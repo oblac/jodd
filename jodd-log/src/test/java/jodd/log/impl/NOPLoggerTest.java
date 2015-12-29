4fa4fa83bf243fd239923d60191d5dbbeb46618f
mfarid <farid.iflex@gmail.com>
2015-12-29 06:08:25 +0530

    Squashed commit of the following:
    commit 43af0314a13b0a74d32237ede5932907e1fcdbe4
    Author: mfarid <farid.iflex@gmail.com>
    Date:   Thu Dec 24 23:36:45 2015 +0530
    
        Squashed commit of the following:
    
        commit 31e095039b633fd55c7c4191e99920c33be831a4
        Author: Charu <Charu>
        Date:   Thu Dec 24 09:04:55 2015 +0530
    
            Incorporated Review Comments
    
        commit bc4c1afca15f747cd3ad0256dd0cb943af074a7b
        Author: Charu <Charu>
        Date:   Mon Dec 21 21:06:39 2015 +0530
    
            devs/vijay/UTS-224 Added COVERAGE_REPORT.md file
    
        commit 1177b921da7b39a5a022c14d661d592c7bc8ffdd
        Author: Charu <Charu>
        Date:   Sun Dec 20 16:44:15 2015 +0530
    
            devs/vijay/UTS-224 Incorporated review comments
    
        commit 01230eb3976210e486cc46758f08d4122af6e3a8
        Author: Charu <Charu>
        Date:   Sun Dec 20 14:29:10 2015 +0530
    
            devs/vijay/UTS-224 Incorporated Review Comments
    
        commit ffec0c9a12092a53b0d339071bdd586531ed0e25
        Author: Charu <Charu>
        Date:   Sat Dec 19 22:50:28 2015 +0530
    
            devs/vijay/UTS-224 Added test cases

---
+++ jodd-log/src/test/java/jodd/log/impl/NOPLoggerTest.java
@@ -0,0 +1,94 @@
+// Copyright (c) 2003-present, Jodd Team (http://jodd.org)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package jodd.log.impl;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import org.junit.Before;
+import org.junit.Test;
+import jodd.log.Logger.Level;
+import jodd.log.impl.util.LoggerConstants;
+
+public class NOPLoggerTest extends LoggerTestBase{
+	
+	private String name="NOPLogger";
+
+	@Before
+	public void setUp() throws Exception {
+		logger =new NOPLogger(name);
+	}
+	
+	@Test
+	public void testIsEnabled(){
+		assertFalse("Source code implemented in such a way that this method call always returns false"
+				,logger.isEnabled(Level.DEBUG));	
+	}
+	
+	@Test
+	public void testGetName(){
+		assertEquals("Name must be equal to NOPLogger", name, logger.getName());
+	}	
+	
+	@Test
+	public void testLog(){
+		//given
+		throwable=mock(Throwable.class);
+		
+		//when
+		//The below methods are no op methods in actual implementations.
+		//so we will not be able to verify anything
+		logger.log(Level.DEBUG, name);
+		logger.trace( name);
+		logger.debug( name);
+		logger.info( name);
+		logger.warn( name);
+		logger.warn( name,throwable);
+		logger.error( name);
+		logger.error( name,throwable);
+	}
+	
+	@Test
+	public void testIsLevelEnabled(){
+		super.testIsLevelEnabled();
+	}
+	
+	@Test
+	public void testJDKLoggerFactory(){
+		//given
+		loggerFactory=new NOPLoggerFactory();
+		
+		//when
+		logger=(NOPLogger)loggerFactory.getLogger(LoggerConstants.LOGGER);
+		
+		//then
+		assertThat("Logger must be of type NOPLogger",logger.getClass(), 
+				is(instanceOf(NOPLogger.class.getClass())));
+	}
+}
