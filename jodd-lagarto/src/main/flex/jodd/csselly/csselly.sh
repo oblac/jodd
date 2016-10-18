05efb76ce51c0fddb2f2e88eef4fdc587ee81e09
Igor Spasić <igor@jodd.org>
2016-10-18 23:25:47 +0200

    Update JFlex to 1.6.1 (closes #349)

--- jodd-lagarto/src/main/flex/jodd/csselly/csselly.sh
+++ jodd-lagarto/src/main/flex/jodd/csselly/csselly.sh
@@ -4,7 +4,7 @@ set -e
 PRJ_ROOT=../../../../../../
 JFLEX_ROOT=$PRJ_ROOT/etc/jflex
 
-java -jar $JFLEX_ROOT/JFlex-1.6.0.jar --skel $JFLEX_ROOT/skeleton.chararray-1.6.0.jodd -d . csselly.flex
+java -jar $JFLEX_ROOT/JFlex-1.6.1.jar --skel $JFLEX_ROOT/skeleton.chararray-1.6.1.jodd -d . csselly.flex
 
 mv CSSellyLexer.java ../../../java/jodd/csselly/
 
