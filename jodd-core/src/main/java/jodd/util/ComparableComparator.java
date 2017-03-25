18dbb231a1d371c7e36f1b13fd0abcce2ec93376
Igor Spasić <igor.spasic@gmail.com>
2017-03-25 22:53:35 +0100

    No need to cache instance.

--- jodd-core/src/main/java/jodd/util/ComparableComparator.java
+++ jodd-core/src/main/java/jodd/util/ComparableComparator.java
@@ -33,11 +33,7 @@ import java.util.Comparator;
  * Comparator that adapts <code>Comparables</code> to the <code>Comparator</code> interface.
  */
 public class ComparableComparator<T extends Comparable<T>> implements Comparator<T>, Serializable {
-	/**
-	 * Cached instance.
-	 */
-	public static final ComparableComparator INSTANCE = new ComparableComparator();
-	
+
 	public int compare(T o1, T o2) {
 		return o1.compareTo(o2);
 	}	
