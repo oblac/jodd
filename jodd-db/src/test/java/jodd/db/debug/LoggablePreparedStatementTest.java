b6033ad2a47cfd116ad423932585e462dd618efc
Igor Spasić <igor.spasic@gmail.com>
2017-08-14 02:00:22 +0200

    DB tests updated

--- jodd-db/src/test/java/jodd/db/debug/LoggablePreparedStatementTest.java
+++ jodd-db/src/test/java/jodd/db/debug/LoggablePreparedStatementTest.java
@@ -41,36 +41,36 @@ public class LoggablePreparedStatementTest extends DbHsqldbTestCase {
 	public void testSetParameters() throws SQLException {
 		Connection connection = cp.getConnection();
 
-		PreparedStatement preparedStatement = LoggablePreparedStatementFactory.create(connection, "select * from BOY t where t.id=?");
+		PreparedStatement preparedStatement = LogabbleStatementFactory.prepared().create(connection, "select * from BOY t where t.id=?");
 		preparedStatement.setInt(1, 7);
 		preparedStatement.close();
 
-		assertEquals("select * from BOY t where t.id=7", LoggablePreparedStatementFactory.getQueryString(preparedStatement));
+		assertEquals("select * from BOY t where t.id=7", LogabbleStatementFactory.prepared().getQueryString(preparedStatement));
 
-		preparedStatement = LoggablePreparedStatementFactory.create(connection, "select * from BOY t where t.name=?");
+		preparedStatement = LogabbleStatementFactory.prepared().create(connection, "select * from BOY t where t.name=?");
 		preparedStatement.setString(1, "jodd");
 		preparedStatement.close();
 
-		assertEquals("select * from BOY t where t.name='jodd'", LoggablePreparedStatementFactory.getQueryString(preparedStatement));
+		assertEquals("select * from BOY t where t.name='jodd'", LogabbleStatementFactory.prepared().getQueryString(preparedStatement));
 
-		preparedStatement = LoggablePreparedStatementFactory.create(connection, "select * from BOY t where t.name=?");
+		preparedStatement = LogabbleStatementFactory.prepared().create(connection, "select * from BOY t where t.name=?");
 		preparedStatement.setNull(1, Types.VARCHAR, "String");
 		preparedStatement.close();
 
-		assertEquals("select * from BOY t where t.name=<null>", LoggablePreparedStatementFactory.getQueryString(preparedStatement));
+		assertEquals("select * from BOY t where t.name=<null>", LogabbleStatementFactory.prepared().getQueryString(preparedStatement));
 
-		preparedStatement = LoggablePreparedStatementFactory.create(connection, "select * from BOY t where t.id=?");
+		preparedStatement = LogabbleStatementFactory.prepared().create(connection, "select * from BOY t where t.id=?");
 		preparedStatement.setFloat(1, 1.73f);
 		preparedStatement.close();
 
-		assertEquals("select * from BOY t where t.id=1.73", LoggablePreparedStatementFactory.getQueryString(preparedStatement));
+		assertEquals("select * from BOY t where t.id=1.73", LogabbleStatementFactory.prepared().getQueryString(preparedStatement));
 
-		preparedStatement = LoggablePreparedStatementFactory.create(connection, "select * from BOY t where t.id=? and t.name=?");
+		preparedStatement = LogabbleStatementFactory.prepared().create(connection, "select * from BOY t where t.id=? and t.name=?");
 		preparedStatement.setString(2, "jodd");
 		preparedStatement.setDouble(1, 1.73);
 		preparedStatement.close();
 
-		assertEquals("select * from BOY t where t.id=1.73 and t.name='jodd'", LoggablePreparedStatementFactory.getQueryString(preparedStatement));
+		assertEquals("select * from BOY t where t.id=1.73 and t.name='jodd'", LogabbleStatementFactory.prepared().getQueryString(preparedStatement));
 
 		cp.closeConnection(connection);
 	}
---
+++ jodd-db/src/testInt/java/jodd/db/oom/PreparedTest.java
@@ -0,0 +1,76 @@
+package jodd.db.oom;
+
+import jodd.db.DbQuery;
+import jodd.db.DbSession;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PreparedTest extends DbBaseTest {
+
+	public class PostgreSql extends PostgreSqlDbAccess {
+		@Override
+		public String getCreateTableSql() {
+			return "create table TESTER (" +
+				"ID			SERIAL," +
+				"NAME		varchar(20)	NOT NULL," +
+				"VALUE		integer NULL," +
+				"primary key (ID)" +
+				')';
+		}
+
+		@Override
+		public String getTableName() {
+			return "TESTER";
+		}
+	}
+
+	@Test
+	public void testPreparedStatementDebugFalse() {
+		DbBaseTest.DbAccess db = new PreparedTest.PostgreSql();
+		init();
+		db.initDb();
+		connect();
+
+		db.createTables();
+		try {
+			test(false);
+		} finally {
+			db.close();
+		}
+	}
+
+	@Test
+	public void testPrepredStatementDebugTrue() {
+		DbBaseTest.DbAccess db = new PreparedTest.PostgreSql();
+		init();
+		db.initDb();
+		connect();
+
+		db.createTables();
+		try {
+			test(true);
+		} finally {
+			db.close();
+		}
+	}
+
+	private void test(final boolean debug) {
+		DbSession session = new DbSession();
+
+		DbQuery dbQuery = new DbQuery(session, "select * from TESTER where id=:id and name=:name");
+		dbQuery.setDebug(debug);
+
+		dbQuery.setInteger("id", 3);
+		dbQuery.setString("name", "John");
+
+		if (debug) {
+			assertEquals("select * from TESTER where id=3 and name='John'", dbQuery.toString());
+		} else {
+			assertEquals("select * from TESTER where id=? and name=?", dbQuery.toString());
+		}
+
+		session.closeSession();
+	}
+
+}
