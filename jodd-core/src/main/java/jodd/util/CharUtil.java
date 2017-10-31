85693e2d63aa6ee0b8a2e5735497d45e6b90ecea
Igor Spasić <igor.spasic@gmail.com>
2017-10-31 23:42:30 +0100

    Added JavaDoc!

--- jodd-core/src/main/java/jodd/util/CharUtil.java
+++ jodd-core/src/main/java/jodd/util/CharUtil.java
@@ -262,6 +262,7 @@ public class CharUtil {
 	/**
 	 * Returns <code>true</code> if character is a white space ({@code <= ' '}).
 	 * White space definition is taken from String class (see: <code>trim()</code>).
+	 * This method has different results then <code>Character#isWhitespace</code>."
 	 */
 	public static boolean isWhitespace(char c) {
 		return c <= ' ';
