2a1b9ca602a5f2bc109d8fed4f8006a1b6472a98
Igor Spasić <igor.spasic@gmail.com>
2017-11-15 21:24:35 +0100

    Fix renaming

--- jodd-petite/src/main/java/jodd/petite/PetiteConfig.java
+++ jodd-petite/src/main/java/jodd/petite/PetiteConfig.java
@@ -35,7 +35,7 @@ public class PetiteConfig {
 		detectDuplicatedBeanNames = false;
 		resolveReferenceParameters = true;
 		useFullTypeNames = false;
-		lookupReferences = PetiteReference.DEFAULT;
+		lookupReferences = PetiteReferenceType.DEFAULT;
 		useParamo = true;
 		wireScopedProxy = false;
 		detectMixedScopes = false;
@@ -143,9 +143,9 @@ public class PetiteConfig {
 
 	// ---------------------------------------------------------------- references
 
-	protected PetiteReference[] lookupReferences;
+	protected PetiteReferenceType[] lookupReferences;
 
-	public PetiteReference[] getLookupReferences() {
+	public PetiteReferenceType[] getLookupReferences() {
 		return lookupReferences;
 	}
 
@@ -153,7 +153,7 @@ public class PetiteConfig {
 	 * Specifies references for bean name lookup, when name
 	 * is not specified, in given order.
 	 */
-	public PetiteConfig setLookupReferences(PetiteReference... lookupReferences) {
+	public PetiteConfig setLookupReferences(PetiteReferenceType... lookupReferences) {
 		this.lookupReferences = lookupReferences;
 		return this;
 	}
