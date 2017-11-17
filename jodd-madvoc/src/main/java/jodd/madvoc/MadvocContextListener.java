11b73695db4a5e3cbcec61e9c649636bba899c8f
Igor Spasić <igor.spasic@gmail.com>
2017-11-17 23:35:58 +0100

    Madvoc Context listener is now the main way to create Madvoc.

--- jodd-madvoc/src/main/java/jodd/madvoc/MadvocContextListener.java
+++ jodd-madvoc/src/main/java/jodd/madvoc/MadvocContextListener.java
@@ -30,7 +30,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 /**
- * Creates {@link Madvoc} @{link WebApplication}. It's an alternative way how to
+ * Creates {@link Madvoc} {@link WebApplication}. It's an alternative way how to
  * start web application - before any other Jodd framework is used and called.
  */
 public class MadvocContextListener implements ServletContextListener {
@@ -38,20 +38,24 @@ public class MadvocContextListener implements ServletContextListener {
 	protected Madvoc madvoc;
 
 	/**
-	 * Creates new {@link Madvoc} @{link WebApplication} that is configured
+	 * Creates new {@link Madvoc} {@link WebApplication} that is configured
 	 * by context init parameters.
 	 */
+	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 		ServletContext servletContext = servletContextEvent.getServletContext();
 
 		madvoc = new Madvoc();
-		madvoc.configure(servletContext);
+
+		madvoc.configureWith(servletContext);
+
 		madvoc.startNewWebApplication(servletContext);
 	}
 
 	/**
 	 * Stops Madvoc.
 	 */
+	@Override
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
 		madvoc.stopWebApplication();
 	}
