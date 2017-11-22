d4d0a6b00a3c3849adac450133a5027079917054
Sascha Fischer <moh.sushi@googlemail.com>
2017-11-22 07:34:01 +0100

    final String -> private final String

--- jodd-core/src/test/java/jodd/util/Base64Test.java
+++ jodd-core/src/test/java/jodd/util/Base64Test.java
@@ -32,11 +32,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class Base64Test {
 
-	final String text = "Man is distinguished, not only by his reason, but by this singular passion from other animals," +
+	private final String text = "Man is distinguished, not only by his reason, but by this singular passion from other animals," +
 			" which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge," +
 			" exceeds the short vehemence of any carnal pleasure.";
 
-	final String enc = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz" +
+	private final String enc = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz" +
 			"IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg" +
 			"dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu" +
 			"dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo" +
