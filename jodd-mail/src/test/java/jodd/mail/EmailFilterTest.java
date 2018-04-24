edb393c59d959472d101b8c2fd19bdb35991a100
Igor Spasić <igor@jodd.org>
2018-04-24 18:37:20 +0200

    Fixing the test and using Longs.

--- jodd-mail/src/test/java/jodd/mail/EmailFilterTest.java
+++ jodd-mail/src/test/java/jodd/mail/EmailFilterTest.java
@@ -170,13 +170,13 @@ class EmailFilterTest {
 	@Test
 	void testReceivedDate() {
 		final EmailFilter emailFilter = EmailFilter.filter()
-			.receivedDate(EmailFilter.Operator.EQ, 1524575533757)
-			.sentDate(EmailFilter.Operator.GT, 1524575533757);
+			.receivedDate(EmailFilter.Operator.EQ, 1524575533757L)
+			.sentDate(EmailFilter.Operator.GT, 1524575533757L);
 
 		final SearchTerm expected =
 			new AndTerm(
-				new ReceivedDateTerm(3, new Date(1524575533757)),
-				new SentDateTerm(5, new Date(1524575533757))
+				new ReceivedDateTerm(3, new Date(1524575533757L)),
+				new SentDateTerm(5, new Date(1524575533757L))
 			);
 
 		assertEquals(expected, emailFilter.searchTerm);
