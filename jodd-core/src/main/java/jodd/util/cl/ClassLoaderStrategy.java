c84bf14c7080d86d227d66da9ba792350f8620b0
Igor Spasić <igor@jodd.org>
2018-05-11 15:44:29 +0200

    Javadoc changed

--- jodd-core/src/main/java/jodd/util/cl/ClassLoaderStrategy.java
+++ jodd-core/src/main/java/jodd/util/cl/ClassLoaderStrategy.java
@@ -34,7 +34,7 @@ import java.util.Objects;
 public interface ClassLoaderStrategy {
 
 	/**
-	 * Default instance of {@link ClassLoaderStrategy}.
+	 * Default implementation of {@link ClassLoaderStrategy}.
 	 */
 	class Implementation {
 
