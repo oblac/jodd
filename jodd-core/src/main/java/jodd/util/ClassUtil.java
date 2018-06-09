4c216070a490b1ebc10d0bc918ce3f94eb3513b4
Igor Spasić <igor@jodd.org>
2018-06-09 19:45:06 +0200

    Added util method for detecting Kotlin classes.

--- jodd-core/src/main/java/jodd/util/ClassUtil.java
+++ jodd-core/src/main/java/jodd/util/ClassUtil.java
@@ -1355,4 +1355,19 @@ public class ClassUtil {
 		return stringBand.toString();
 	}
 
+	// ---------------------------------------------------------------- kotlin
+
+	/**
+	 * Returns {@code true} if type is a Kotlin class.
+	 */
+	public static boolean isKotlinClass(final Class type) {
+		final Annotation[] annotations = type.getAnnotations();
+		for (Annotation annotation : annotations) {
+			if (annotation.annotationType().getName().equals("kotlin.Metadata")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
