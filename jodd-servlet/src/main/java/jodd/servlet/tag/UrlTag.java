72399d6f830fef5426af753e03da5ac179ed51f5
Igor Spasić <igor@jodd.org>
2018-06-10 23:20:56 +0200

    Using new method.

--- jodd-servlet/src/main/java/jodd/servlet/tag/UrlTag.java
+++ jodd-servlet/src/main/java/jodd/servlet/tag/UrlTag.java
@@ -76,12 +76,12 @@ public class UrlTag extends SimpleTagSupport implements DynamicAttributes {
 		if (var == null) {
 			JspWriter out = pageContext.getOut();
 			try {
-				out.print(builder.toString());
+				out.print(builder.get());
 			} catch (IOException ioex) {
 				// ignore
 			}
 		} else {
-			pageContext.setAttribute(var, builder.toString());
+			pageContext.setAttribute(var, builder.get());
 		}
 	}
 }
