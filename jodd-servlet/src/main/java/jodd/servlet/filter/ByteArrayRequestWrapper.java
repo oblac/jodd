0cc0ad529f522c4646071bfa71fd392da5f5effd
Igor Spasić <igor.spasic@gmail.com>
2020-02-27 22:02:37 +0100

    Added ByteArrayRequestWrapper

---
+++ jodd-servlet/src/main/java/jodd/servlet/filter/ByteArrayRequestWrapper.java
@@ -0,0 +1,61 @@
+// Copyright (c) 2003-present, Jodd Team (http://jodd.org)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package jodd.servlet.filter;
+
+import jodd.io.StreamUtil;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Simple request wrapper that can be re-used multiple times.
+ */
+public class ByteArrayRequestWrapper extends HttpServletRequestWrapper {
+	private final byte[] body;
+
+	public ByteArrayRequestWrapper(final HttpServletRequest request) throws IOException {
+		super(request);
+		final InputStream requestInputStream = request.getInputStream();
+		this.body = StreamUtil.readBytes(requestInputStream);
+	}
+
+	@Override
+	public ServletInputStream getInputStream() {
+		return new ByteArrayServletInputStream(this.body);
+	}
+
+	@Override
+	public BufferedReader getReader() {
+		final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.body);
+		return new BufferedReader(new InputStreamReader(byteArrayInputStream));
+	}
+}
